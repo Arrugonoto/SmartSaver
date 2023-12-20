b4815e757f68a3a548c44e75dd5a36a72ece80e8
Arugonoto <klekot.krzysztof1008@gmail.com>
2023-12-20 21:28:45 +0100

    update: added route protection with redirect when user session is available

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,15 @@
 import LoginForm from '@/components/forms/LoginForm';
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
-export default function Login() {
+export default async function Login() {
+   const session = await getServerSession(authOptions);
+
+   if (session) {
+      redirect('/dashboard');
+   }
+
    return (
       <main className="h-screen">
          <section className="flex flex-col w-full justify-center items-center h-full">
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,15 @@
 import RegisterForm from '@/components/forms/RegisterForm';
+import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
-export default function Signup() {
+export default async function Signup() {
+   const session = await getServerSession(authOptions);
+
+   if (session) {
+      redirect('/dashboard');
+   }
+
    return (
       <main className="h-screen">
          <section className="flex flex-col w-full justify-center items-center h-full">
