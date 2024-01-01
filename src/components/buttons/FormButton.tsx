4866bd7a728048d07a71e7f00769db8f9dc3e1e4
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-01-01 21:33:57 +0100

    udpate(FormButton): added type and property for button disabled state based on passed prop value

--- src/components/buttons/FormButton.tsx
+++ src/components/buttons/FormButton.tsx
@@ -4,15 +4,23 @@ import { Button } from '@nextui-org/button';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
    children: React.ReactNode;
    onPress?: () => void;
+   isDisabled?: boolean;
 }
 
-const FormButton = ({ children, className, type, onPress }: ButtonProps) => {
+const FormButton = ({
+   children,
+   className,
+   type,
+   onPress,
+   isDisabled,
+}: ButtonProps) => {
    return (
       <Button
          radius="full"
          type={type ?? 'button'}
          className={`w-full ${className}`}
          onPress={() => onPress && onPress()}
+         isDisabled={isDisabled}
       >
          {children}
       </Button>
