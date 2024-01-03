0cbbb23a1eb0c917ad92e2ab4caccf1df9e96aa7
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-01-03 12:38:25 +0100

    rearrange(definitions): added file with database data type definitions

---
+++ src/lib/definitions.ts
@@ -0,0 +1,23 @@
+// Type definitions for data inside of Database
+
+export type User = {
+   id: string;
+   name: string;
+   email: string;
+   password: string;
+   profile_image_url: string;
+};
+
+export type UsersExpenses = {
+   id: string;
+   user_id: string;
+   amount: number;
+};
+
+export type Expense = {
+   id: string;
+   user_id: string;
+   name: string;
+   amount: number;
+   type: string;
+};
