8ff22c30dfd6bac62478ef5bdb2dd42b605378df
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-01-03 12:36:58 +0100

    update(auth): added credentials register handling(currently for front side only, will be fixed later)
    handling registration via server side will be added later, also type validation with zod will be added too

--- src/auth.ts
+++ src/auth.ts
@@ -7,6 +7,20 @@ import type { NextAuthOptions } from 'next-auth';
 import { getServerSession } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { sql } from '@vercel/postgres';
+import type { User } from '@/lib/definitions';
+import { compare } from 'bcrypt';
+
+async function getUser(email: string): Promise<User | undefined> {
+   try {
+      const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+      return user.rows[0];
+   } catch (error) {
+      console.error('Failed to fetch user:', error);
+      throw new Error(`User with that email address doesn't exists.`);
+   }
+}
 
 export const authConfig = {
    pages: {
@@ -14,6 +28,29 @@ export const authConfig = {
       signOut: '/',
    },
    providers: [
+      CredentialsProvider({
+         credentials: {
+            username: {},
+            email: {},
+            password: {},
+         },
+         async authorize(credentials, req) {
+            let user;
+
+            if (credentials) {
+               user = await getUser(credentials.email);
+
+               if (!user) return null;
+               const passwordsMatch = await compare(
+                  credentials?.password,
+                  user.password
+               );
+               if (passwordsMatch) return user;
+            }
+
+            return null;
+         },
+      }),
       GitHubProvider({
          clientId: process.env.GITHUB_ID as string,
          clientSecret: process.env.GITHUB_SECRET as string,
