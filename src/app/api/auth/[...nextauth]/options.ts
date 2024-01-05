25e804b38f09123122d69e954d0acde889f8bf35
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-01-05 12:46:47 +0100

    rearrange/refactor: moved next-auth options configuration to new file(issue with export)

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,6 @@
+import type { NextAuthOptions } from 'next-auth';
+import { authConfig } from '@/auth';
+
+export const authOptions: NextAuthOptions = {
+   ...authConfig,
+};
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,5 @@
 import NextAuth from 'next-auth';
-import type { NextAuthOptions } from 'next-auth';
-import { authConfig } from '@/auth';
-
-export const authOptions: NextAuthOptions = {
-   ...authConfig,
-};
+import { authOptions } from '@/app/api/auth/[...nextauth]/options';
 
 const handler = NextAuth(authOptions);
 
