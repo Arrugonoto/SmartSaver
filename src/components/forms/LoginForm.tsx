25e3203e69914354c1b1bd1fe2669899671f3e5a
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-01-10 20:41:28 +0100

    update(LoginForm): added form state handling with react useFormState

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -2,10 +2,11 @@
 import React, { useMemo } from 'react';
 import { useState } from 'react';
 import { Input } from '@nextui-org/input';
-import { Button } from '@nextui-org/button';
 import { signIn } from 'next-auth/react';
 import Link from 'next/link';
 import FormButton from '@/components/buttons/FormButton';
+import { useFormState, useFormStatus } from 'react-dom';
+import { authenticate } from '@/lib/actions';
 
 type FormDataType = {
    email: string;
@@ -17,6 +18,8 @@ const LoginForm = () => {
       email: '',
       password: '',
    });
+   const [state, dispatch] = useFormState(authenticate, undefined);
+   const { pending } = useFormStatus();
 
    const validateEmail = (formData: FormDataType) =>
       formData.email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
@@ -35,7 +38,7 @@ const LoginForm = () => {
    return (
       <div className="w-1/4 min-w-[20rem]">
          <form
-            action={``}
+            action={dispatch}
             className="flex flex-col w-full bg-gray-900 px-4 md:px-8 py-6 rounded-lg transition-all"
          >
             <h1 className="text-2xl text-center mb-6">Sign In</h1>
@@ -64,7 +67,9 @@ const LoginForm = () => {
                   value={formData.password}
                   onChange={e => handleChange(e)}
                />
-               <FormButton type="submit">Login</FormButton>
+               <FormButton type="submit" isDisabled={pending}>
+                  Login
+               </FormButton>
             </div>
             <div className="relative h-0.5 mt-8 mb-6 bg-black">
                <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-900 px-3">
@@ -77,6 +82,7 @@ const LoginForm = () => {
                   onPress={() =>
                      signIn('github', { callbackUrl: '/dashboard' })
                   }
+                  isDisabled={pending}
                >
                   GitHub
                </FormButton>
@@ -84,12 +90,13 @@ const LoginForm = () => {
                   onPress={() =>
                      signIn('google', { callbackUrl: '/dashboard' })
                   }
+                  isDisabled={pending}
                >
                   Google
                </FormButton>
             </div>
             <p className="text-sm pt-6 pb-2 text-center">
-               {`Don't have an account?`}{' '}
+               {`Don't have an account? `}
                <span className="relative text-green-400 after:content-[''] after:absolute after:w-full after:h-px after:bg-transparent after:left-0 after:-bottom-0.5 hover:after:bg-green-400 after:transition-all">
                   <Link href="/signup">Create Now</Link>
                </span>
