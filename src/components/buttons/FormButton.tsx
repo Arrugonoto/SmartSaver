8a3bfbafe22486700c29f7757e646bbe8145c25e
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-01-14 23:38:17 +0100

    update(FormButton): removed useFormState hook
    useFormState works only with native form action events as it is still in a canary version(experimental feature) and requires to use server components, it's okay to use now but triggers native page reload

--- src/components/buttons/FormButton.tsx
+++ src/components/buttons/FormButton.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { Button } from '@nextui-org/button';
-import { useFormStatus } from 'react-dom';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
    children: React.ReactNode;
@@ -15,15 +14,13 @@ const FormButton = ({
    onPress,
    isDisabled,
 }: ButtonProps) => {
-   const { pending } = useFormStatus();
-
    return (
       <Button
          radius="full"
          type={type ?? 'button'}
          className={`w-full ${className}`}
          onPress={() => onPress && onPress()}
-         isDisabled={isDisabled || pending}
+         isDisabled={isDisabled}
       >
          {children}
       </Button>
