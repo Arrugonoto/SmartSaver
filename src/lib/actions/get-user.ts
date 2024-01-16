5160c2f6da4c7de29062cb9c4b48a9c4c0de590d
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-01-16 20:39:58 +0100

    rearrange(actions): moved server actions into separate appropriate files

---
+++ src/lib/actions/get-user.ts
@@ -0,0 +1,18 @@
+'use server';
+import { sql } from '@vercel/postgres';
+import type { User } from '@/lib/definitions';
+
+export async function getUser(email: string): Promise<User | undefined> {
+   try {
+      const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+
+      if (!user.rows[0]) {
+         throw Error(`User with that email address doesn't exist!`);
+      }
+      return user.rows[0];
+   } catch (error) {
+      const errorMessage =
+         error instanceof Error ? error.message : 'Unknown error';
+      throw { error: error, message: errorMessage };
+   }
+}
