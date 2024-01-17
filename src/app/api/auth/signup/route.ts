6bf508cf4d086432cc95d88444029448c8a7740e
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-01-17 21:17:15 +0100

    rearrange(signup): added route for user registration

---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { createUser } from '@/lib/actions/create-user';
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+   try {
+      const { name, email, password } = await req.body;
+      // validate email and password - Zod will be used here
+
+      console.log(name, email, password);
+
+      return NextResponse.json(
+         { message: 'Account created succesfully' },
+         { status: 200 }
+      );
+   } catch (e) {
+      console.error('Sign-in error:', e);
+      return NextResponse.json(
+         { error: e, message: 'Internal Server Error. Registration failed.' },
+         { status: 500 }
+      );
+   }
+}
