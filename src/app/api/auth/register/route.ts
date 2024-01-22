886e3c5d1555f6185b63082c8ab9626b2476c8f0
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-01-22 06:36:23 +0100

    fix(api/register): fixed issue with wrongly handled server response

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,21 +1,26 @@
 import { NextResponse } from 'next/server';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiResponse } from 'next';
 import { createUser } from '@/lib/actions/create-user';
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(req: Request, res: NextApiResponse) {
    try {
-      const { name, email, password } = await req.body;
+      const formData = await req.json();
       // validate email and password - Zod will be used here
-      console.log(name, email, password);
 
+      const res = await createUser(formData);
+      if (!res?.error)
+         return NextResponse.json(
+            { message: 'Account created succesfully' },
+            { status: 200 }
+         );
+      return NextResponse.json({ message: res.message }, { status: 500 });
+   } catch (error) {
+      console.error('Sign-up error:', error);
       return NextResponse.json(
-         { message: 'Account created succesfully' },
-         { status: 200 }
-      );
-   } catch (e) {
-      console.error('Sign-up error:', e);
-      return NextResponse.json(
-         { error: e, message: 'Internal Server Error. Registration failed.' },
+         {
+            error: error,
+            message: 'Internal Server Error. Registration failed.',
+         },
          { status: 500 }
       );
    }
