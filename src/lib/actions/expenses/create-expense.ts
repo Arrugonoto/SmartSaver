0446affe7b3f0d14ec3f17e1a618d78b2b279a4c
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-02-03 23:53:18 +0100

    feat: added action for creating new expense(not-tested)

---
+++ src/lib/actions/expenses/create-expense.ts
@@ -0,0 +1,42 @@
+'use server';
+import { sql } from '@vercel/postgres';
+import { z } from 'zod';
+import type { Expense } from '@/lib/definitions';
+
+const ExpenseSchema = z.object({
+   user_id: z.string(),
+   name: z.string(),
+   amount: z.number(),
+   type: z.string(),
+});
+
+export async function createExpense(formData: {
+   user_id: string;
+   name: string;
+   amount: number;
+   type: string;
+}) {
+   const validatedFields = ExpenseSchema.safeParse({
+      user_id: formData.user_id,
+      name: formData.name,
+      amount: formData.amount,
+      type: formData.type,
+   });
+
+   if (!validatedFields.success) {
+      throw new Error(' Fill all necessary fields.');
+   }
+
+   const { user_id, name, amount, type } = validatedFields.data;
+
+   try {
+      const createNewExpense = await sql`
+      INSERT INTO expenses (user_id, name, amount, type)
+      VALUES (${user_id}, ${name}, ${amount}, ${type},)
+      `;
+   } catch (error) {
+      const errorMessage = error instanceof Error && error.message;
+      console.log(error);
+      return { error: error, message: errorMessage };
+   }
+}
