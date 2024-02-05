8b8185298ac4b995b82f53daf9fef6fd6ac6b7b1
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-02-05 13:19:41 +0100

    feat: added action for getting user related expenses

---
+++ src/lib/actions/expenses/get-expenses.ts
@@ -0,0 +1,18 @@
+'use server';
+import { sql } from '@vercel/postgres';
+import type { Expense } from '@/lib/definitions';
+
+export async function getExpenses(user_id: string) {
+   try {
+      const userExpenses = await sql<Expense>`
+      SELECT * FROM expenses WHERE user_id=${user_id} `;
+
+      if (userExpenses.rows.length === 0) {
+         throw new Error(`You have no expenses to keep track of`);
+      }
+   } catch (error) {
+      const errorMessage = error instanceof Error && error.message;
+      console.log(error);
+      return { error: error, message: errorMessage };
+   }
+}
