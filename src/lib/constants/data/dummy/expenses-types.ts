4bac7491dc43387e84cd9d9c9426511ca40d4735
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-02-11 01:56:28 +0100

    rearrange: added file with dummy data for expenses types

---
+++ src/lib/constants/data/dummy/expenses-types.ts
@@ -0,0 +1,38 @@
+export const expenseTypesList: ExpenseTypes[] = [
+   {
+      label: 'Basic',
+      value: 'basic-expenses',
+   },
+   {
+      label: 'Beauty',
+      value: 'beauty',
+   },
+   {
+      label: 'Bills',
+      value: 'bills',
+   },
+   {
+      label: 'Car & Transport',
+      value: 'car-and-transport',
+   },
+   {
+      label: 'Clothing & Footwear',
+      value: 'clothing-and-footwear',
+   },
+   {
+      label: 'Entertainment',
+      value: 'entertainment',
+   },
+   {
+      label: 'Healthcare',
+      value: 'healthcare',
+   },
+   {
+      label: 'Self Improvement',
+      value: 'self-improvement',
+   },
+   {
+      label: 'Travel',
+      value: 'travel',
+   },
+];
