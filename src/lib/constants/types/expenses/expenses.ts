82b33fc09324cb3a6b75bf4eba94986b96d87e35
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-02-13 22:10:07 +0100

    update: added types for expense object

--- src/lib/constants/types/expenses/expenses.ts
+++ src/lib/constants/types/expenses/expenses.ts
@@ -1,4 +1,12 @@
-type ExpenseTypes = {
+export type Expense = {
+   user_id: string;
+   name: string;
+   amount: number;
+   expense_type: string;
+   payment_type: 'single' | 'monthly';
+};
+
+export type ExpenseTypes = {
    label: string;
    value: string;
 };
