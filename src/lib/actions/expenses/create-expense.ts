a64bda2872c23a42c649720adb17f7649019e38f
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-02-17 23:57:03 +0100

    update: added payment_type to expense schema

--- src/lib/actions/expenses/create-expense.ts
+++ src/lib/actions/expenses/create-expense.ts
@@ -7,32 +7,38 @@ const ExpenseSchema = z.object({
    user_id: z.string(),
    name: z.string(),
    amount: z.number(),
-   type: z.string(),
+   expense_type: z.string(),
+   payment_type: z.string(),
 });
 
 export async function createExpense(formData: {
    user_id: string;
    name: string;
    amount: number;
-   type: string;
+   expense_type: string;
+   payment_type: string;
 }) {
+   console.log(formData);
+
    const validatedFields = ExpenseSchema.safeParse({
       user_id: formData.user_id,
       name: formData.name,
       amount: formData.amount,
-      type: formData.type,
+      expense_type: formData.expense_type,
+      payment_type: formData.payment_type,
    });
 
    if (!validatedFields.success) {
       throw new Error(' Fill all necessary fields.');
    }
 
-   const { user_id, name, amount, type } = validatedFields.data;
+   const { user_id, name, amount, expense_type, payment_type } =
+      validatedFields.data;
 
    try {
       const createNewExpense = await sql<Expense>`
       INSERT INTO expenses (user_id, name, amount, type)
-      VALUES (${user_id}, ${name}, ${amount}, ${type},)
+      VALUES (${user_id}, ${name}, ${amount}, ${expense_type}, ${payment_type})
       `;
    } catch (error) {
       const errorMessage = error instanceof Error && error.message;
