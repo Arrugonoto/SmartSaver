00f58cf2ab35087c786f22f262bafb3d5e84420d
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-02-18 20:10:36 +0100

    fix(expense-form): fixed issue with wrongly passed form data value types

--- src/components/forms/expenses-form/index.tsx
+++ src/components/forms/expenses-form/index.tsx
@@ -1,38 +1,61 @@
 'use client';
-import type { Expense } from '@lib/definitions';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Select, SelectItem } from '@nextui-org/select';
 import { Input } from '@nextui-org/input';
 import { expenseTypesList } from '@data/dummy/expenses-types';
+import { Expense } from '@constants/types/expenses/expenses';
+import FormButton from '@components/buttons/FormButton';
+import { createExpense } from '@lib/actions/expenses/create-expense';
 
-export const ExpensesForm = () => {
-   const [paymentType, setPaymentType] = useState<'single' | 'subscription'>(
-      'single'
-   );
-   const [formData, setFormData] = useState<Expense>({
-      id: '',
-      user_id: '',
-      name: '',
-      amount: 0,
-      type: paymentType,
-   });
+export const ExpensesForm = ({ user_id }: { user_id: string }) => {
+   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const formData = new FormData(e.currentTarget);
+      const amountValue = formData.get('amount') as string;
+      const amount = amountValue ? parseInt(amountValue) : 0;
+      const expenseData = { ...Object.fromEntries(formData), amount };
+      console.log(expenseData);
+
+      const newExpense = {
+         user_id,
+         ...expenseData,
+      } as Expense;
+      console.log(newExpense);
+
+      await createExpense(newExpense);
+   };
 
    return (
-      <div className="flex bg-gray-900 p-2 rounded-md">
-         <form className="flex flex-col gap-3 min-w-[200px] w-[320px]">
-            <Input type="text" label="Expense name" />
-            <Input type="number" label="Expense amount" />
-            <Select label="Expense type">
+      <div className="flex p-2 rounded-md">
+         <form onSubmit={handleSubmit} className="flex flex-col gap-3 w-full">
+            <Input type="text" name="name" label="Expense name" />
+            <Input type="number" name="amount" label="Expense amount" min={1} />
+            <Select
+               label="Expense type"
+               name="expense_type"
+               placeholder="Select expense type"
+            >
                {expenseTypesList.map(expense => (
                   <SelectItem key={expense.value} value={expense.value}>
                      {expense.label}
                   </SelectItem>
                ))}
             </Select>
-            <Select label="Payment type">
-               <SelectItem key="single">Single</SelectItem>
-               <SelectItem key="monthly">Monthly</SelectItem>
+            <Select
+               label="Payment type"
+               name="payment_type"
+               placeholder="Select payment type"
+            >
+               <SelectItem key="single" value={'single'}>
+                  Single
+               </SelectItem>
+               <SelectItem key="monthly" value={'monthly'}>
+                  Monthly
+               </SelectItem>
             </Select>
+            <FormButton type="submit" color="primary">
+               Create Expense
+            </FormButton>
          </form>
       </div>
    );
