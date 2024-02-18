02b40a64ce8daf7baea9e069763385346cd0a8ff
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-02-18 20:14:59 +0100

    update(expenses): updated types for Expense

--- src/lib/constants/types/expenses/expenses.ts
+++ src/lib/constants/types/expenses/expenses.ts
@@ -3,7 +3,7 @@ export type Expense = {
    name: string;
    amount: number;
    expense_type: string;
-   payment_type: 'single' | 'monthly';
+   payment_type: 'single' | 'monthly' | 'subscription';
 };
 
 export type ExpenseTypes = {
