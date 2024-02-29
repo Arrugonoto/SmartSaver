20d6c383823949ede832db2997ad70b69a5a9c68
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-02-29 23:59:28 +0100

    refactor: removed formData API, added controlled form

--- src/components/forms/expenses-form/index.tsx
+++ src/components/forms/expenses-form/index.tsx
@@ -8,32 +8,72 @@ import FormButton from '@components/buttons/FormButton';
 import { createExpense } from '@lib/actions/expenses/create-expense';
 
 export const ExpensesForm = ({ user_id }: { user_id: string }) => {
+   const [formData, setFormData] = useState<Expense>({
+      user_id: user_id,
+      name: '',
+      amount: 0,
+      expense_type: '',
+      payment_type: 'single',
+   });
+
+   const resetForm = () => {
+      setFormData({
+         user_id: user_id,
+         name: '',
+         amount: 0,
+         expense_type: '',
+         payment_type: 'single',
+      });
+   };
+
+   const handleChange = (
+      e:
+         | React.ChangeEvent<HTMLSelectElement>
+         | React.ChangeEvent<HTMLInputElement>
+   ) => {
+      setFormData(prev => ({
+         ...prev,
+         [e.target.name]: e.target.value,
+      }));
+   };
+
    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-      const formData = new FormData(e.currentTarget);
-      const amountValue = formData.get('amount') as string;
-      const amount = amountValue ? parseInt(amountValue) : 0;
-      const expenseData = { ...Object.fromEntries(formData), amount };
-      console.log(expenseData);
 
-      const newExpense = {
-         user_id,
-         ...expenseData,
-      } as Expense;
-      console.log(newExpense);
+      console.log(formData);
 
-      await createExpense(newExpense);
+      // await createExpense(newExpense);
+      // resetForm();
    };
 
    return (
       <div className="flex p-2 rounded-md">
          <form onSubmit={handleSubmit} className="flex flex-col gap-3 w-full">
-            <Input type="text" name="name" label="Expense name" />
-            <Input type="number" name="amount" label="Expense amount" min={1} />
+            <Input
+               type="text"
+               name="name"
+               label="Expense name"
+               value={formData.name}
+               isRequired
+               onChange={e => handleChange(e)}
+            />
+            <Input
+               type="number"
+               name="amount"
+               label="Expense amount"
+               value={formData.amount.toString()}
+               min={1}
+               isRequired
+               onChange={e => handleChange(e)}
+            />
             <Select
                label="Expense type"
                name="expense_type"
                placeholder="Select expense type"
+               selectedKeys={[`${formData.expense_type}`]}
+               disabledKeys={['']}
+               isRequired
+               onChange={e => handleChange(e)}
             >
                {expenseTypesList.map(expense => (
                   <SelectItem key={expense.value} value={expense.value}>
@@ -45,7 +85,14 @@ export const ExpensesForm = ({ user_id }: { user_id: string }) => {
                label="Payment type"
                name="payment_type"
                placeholder="Select payment type"
+               selectedKeys={[`${formData.payment_type}`]}
+               disabledKeys={['none']}
+               isRequired
+               onChange={e => handleChange(e)}
             >
+               <SelectItem key="none" value={''}>
+                  Select value
+               </SelectItem>
                <SelectItem key="single" value={'single'}>
                   Single
                </SelectItem>
