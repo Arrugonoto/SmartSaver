3c5c986e8e3cedf4029e8cf2c3abfca7fea4a844
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-03-04 23:58:50 +0100

    update: improved way of returned quered data from db

--- src/lib/actions/expenses/get-expenses.ts
+++ src/lib/actions/expenses/get-expenses.ts
@@ -5,14 +5,16 @@ import type { Expense } from '@lib/definitions';
 export async function getExpenses(user_id: string) {
    try {
       const userExpenses = await sql<Expense>`
-      SELECT * FROM expenses WHERE user_id=${user_id} `;
+      SELECT id, name, amount, expense_type, payment_type, created_At, updated_At FROM expenses WHERE user_id=${user_id}`;
 
       if (userExpenses.rows.length === 0) {
          throw new Error(`You have no expenses to keep track of`);
       }
+
+      return { data: userExpenses.rows, status: 'success' };
    } catch (error) {
       const errorMessage = error instanceof Error && error.message;
       console.log(error);
-      return { error: error, message: errorMessage };
+      return { error: error, message: errorMessage, status: 'fail' };
    }
 }
