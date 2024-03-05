0ae055a16006bf90b2bc67246c5f8e09b5f1a414
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-03-05 20:31:52 +0100

    update(expenses-page): added table with data about user related expenses

--- src/app/dashboard/expenses/page.tsx
+++ src/app/dashboard/expenses/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@app/api/auth/[...nextauth]/options';
 import { CreateExpenseModal } from '@components/modals/create-expense-modal';
+import { ExpensesSection } from '@components/sections/expenses-section';
 
 export const metadata: Metadata = {
    title: 'Expenses',
@@ -12,8 +13,15 @@ export default async function Expenses() {
    const user_id = session?.user.id;
 
    return (
-      <main className="flex flex-1 flex-col">
-         <CreateExpenseModal user_id={user_id} />
+      <main className="flex flex-1 flex-col px-4 py-2">
+         <div className="flex flex-col gap-4">
+            <div className="flex justify-between">
+               <h1>Expenses</h1>
+               <CreateExpenseModal user_id={user_id} />
+            </div>
+
+            <ExpensesSection user_id={user_id} />
+         </div>
       </main>
    );
 }
