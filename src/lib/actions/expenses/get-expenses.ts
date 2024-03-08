d9f686c85b59260741d84a2b5cb354dd6d9a3f85
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-03-08 22:00:55 +0100

    update/feat(expenses action): added server side pagination

--- src/lib/actions/expenses/get-expenses.ts
+++ src/lib/actions/expenses/get-expenses.ts
@@ -2,16 +2,38 @@
 import { sql } from '@vercel/postgres';
 import type { Expense } from '@lib/definitions';
 
-export async function getExpenses(user_id: string) {
+export async function getExpenses(user_id: string, page: number) {
+   // change this value to limit returned results from query
+   // as default 20
+   const resultsLimit = 2;
+
    try {
       const userExpenses = await sql<Expense>`
-      SELECT id, name, amount, expense_type, payment_type, description, created_at, updated_at FROM expenses WHERE user_id=${user_id}`;
+      SELECT id, name, amount, expense_type, payment_type, description, created_at, updated_at 
+      FROM expenses 
+      WHERE user_id=${user_id}
+      LIMIT ${resultsLimit}
+      OFFSET ${(page - 1) * 2}
+      `;
+
+      const pagesInDB = await sql`
+      SELECT COUNT(*) AS total_pages
+FROM expenses
+WHERE user_id = ${user_id}`;
+
+      const totalPages = Math.ceil(
+         pagesInDB.rows[0].total_pages / resultsLimit
+      );
 
       if (userExpenses.rows.length === 0) {
          return { message: `You have no expenses to keep track of` };
       }
 
-      return { data: userExpenses.rows, status: 'success' };
+      return {
+         data: userExpenses.rows,
+         totalPages: totalPages,
+         status: 'success',
+      };
    } catch (error) {
       const errorMessage = error instanceof Error && error.message;
       console.log(error);
