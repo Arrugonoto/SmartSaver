04c62b0a69cc652d2462e50c83e21d44cb913346
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-03-11 22:05:20 +0100

    feat(expenses-section): added sorting functionality for table columns

--- src/components/sections/expenses-section/index.tsx
+++ src/components/sections/expenses-section/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import {
    Table,
    TableHeader,
@@ -8,6 +8,7 @@ import {
    TableRow,
    TableCell,
    getKeyValue,
+   SortDescriptor,
 } from '@nextui-org/table';
 import { Spinner } from '@nextui-org/spinner';
 import {
@@ -36,6 +37,10 @@ export const ExpensesSection = ({ user_id }: { user_id: string }) => {
    const [data, setData] = useState<Expense[]>([]);
    const [page, setPage] = useState<number>(1);
    const [totalPages, setTotalPages] = useState<number>(1);
+   const [sortDescriptor, setSortDescriptor] = useState<SortDescriptor>({
+      column: 'name',
+      direction: 'ascending',
+   });
 
    const fetchExpenses = async () => {
       setIsLoading(true);
@@ -50,6 +55,20 @@ export const ExpensesSection = ({ user_id }: { user_id: string }) => {
       setIsLoading(false);
    };
 
+   const sortedData = useMemo(() => {
+      const sortData = data.sort((a, b) => {
+         const first = a[sortDescriptor.column as keyof Expense] as number;
+         const next = b[sortDescriptor.column as keyof Expense] as number;
+         const sortResult = first < next ? -1 : first > next ? 1 : 0;
+
+         return sortDescriptor.direction === 'descending'
+            ? -sortResult
+            : sortResult;
+      });
+
+      return sortData;
+   }, [sortDescriptor, data]);
+
    useEffect(() => {
       fetchExpenses();
       // eslint-disable-next-line
@@ -57,9 +76,10 @@ export const ExpensesSection = ({ user_id }: { user_id: string }) => {
 
    return (
       <section className="w-full flex-1">
-         <div className="flex flex-col h-full flex-1 gap-4 overflow-hidden">
+         <div className="flex w-full flex-col h-full flex-1 gap-4 overflow-hidden">
             <Table
                aria-label="User related expenses table"
+               sortDescriptor={sortDescriptor}
                selectionMode="single"
                color="primary"
                bottomContent={
@@ -76,16 +96,24 @@ export const ExpensesSection = ({ user_id }: { user_id: string }) => {
                      </div>
                   )
                }
+               onSortChange={setSortDescriptor}
             >
                <TableHeader columns={columns}>
                   {column => (
-                     <TableColumn key={column.key}>{column.label}</TableColumn>
+                     <TableColumn
+                        key={column.key}
+                        allowsSorting={column.key != 'actions'}
+                     >
+                        {column.label}
+                     </TableColumn>
                   )}
                </TableHeader>
                <TableBody
-                  items={data}
+                  items={sortedData}
                   emptyContent={
-                     "Currently You don't have any expenses to keep track on."
+                     isLoading
+                        ? ''
+                        : "Currently You don't have any expenses to keep track on."
                   }
                   loadingContent={<Spinner />}
                   loadingState={isLoading ? 'loading' : 'idle'}
