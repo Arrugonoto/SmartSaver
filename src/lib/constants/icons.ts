7fd23bf36f795c360b9b3225a843c0b30edc3de1
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-03-18 23:37:14 +0100

    rearrange(icons): added file with icons used in project

---
+++ src/lib/constants/icons.ts
@@ -0,0 +1,7 @@
+import { FaRegTrashCan, FaEllipsisVertical, FaPencil } from 'react-icons/fa6';
+
+export const tableIcons = {
+   delete: FaRegTrashCan,
+   menu: FaEllipsisVertical,
+   edit: FaPencil,
+};
