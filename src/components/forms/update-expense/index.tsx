8a0ccc4976ab4bb70ec79ab271de15023ace0b6c
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-03-24 23:58:59 +0100

    feat(update-expense): added form with functionality to update selected expense

---
+++ src/components/forms/update-expense/index.tsx
@@ -0,0 +1,129 @@
+'use client';
+import React, { useState } from 'react';
+import { Select, SelectItem } from '@nextui-org/select';
+import { Input } from '@nextui-org/input';
+import { expenseTypesList } from '@lib/constants/data/dummy/expense-values';
+import { Expense } from '@constants/types/expenses/expenses';
+import FormButton from '@components/buttons/FormButton';
+import { updateExpense } from '@lib/actions/expenses/update-expense';
+import { Textarea } from '@nextui-org/input';
+
+export const UpdateExpenseForm = ({ expense }: { expense: Expense }) => {
+   const [formData, setFormData] = useState<
+      Omit<Expense, 'user_id'> & { user_id?: string }
+   >({
+      id: expense.id,
+      name: '',
+      amount: 0,
+      expense_type: '',
+      payment_type: '',
+      description: '',
+   });
+
+   const resetForm = () => {
+      setFormData(prev => ({
+         ...prev,
+         name: '',
+         amount: 0,
+         expense_type: '',
+         payment_type: '',
+         description: '',
+      }));
+   };
+
+   const handleChange = (
+      e:
+         | React.ChangeEvent<HTMLSelectElement>
+         | React.ChangeEvent<HTMLInputElement>
+   ) => {
+      setFormData(prev => ({
+         ...prev,
+         [e.target.name]: e.target.value,
+      }));
+   };
+
+   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      // sometime TS is a conversion hell, knows that 'amount' should be number but,
+      // select makes it a string... I'm converting 'string to string' to be able to convert it to float...
+      const { amount } = formData;
+      const stringAmount = amount.toString();
+      const expenseAmount = parseFloat(stringAmount);
+      const expenseData = {
+         ...formData,
+         amount: expenseAmount,
+      };
+
+      await updateExpense(expenseData);
+      resetForm();
+   };
+
+   return (
+      <div className="flex p-2 rounded-md w-full">
+         <form onSubmit={handleSubmit} className="flex flex-col gap-3 w-full">
+            <Input
+               type="text"
+               name="name"
+               label="Expense name"
+               value={formData.name}
+               isRequired
+               onChange={e => handleChange(e)}
+            />
+            <Input
+               type="number"
+               name="amount"
+               label="Expense amount"
+               value={formData.amount.toString()}
+               step="0.01"
+               min={1}
+               isRequired
+               onChange={e => handleChange(e)}
+            />
+            <Select
+               label="Expense type"
+               name="expense_type"
+               placeholder="Select expense type"
+               selectedKeys={[`${formData.expense_type}`]}
+               disabledKeys={['']}
+               isRequired
+               onChange={e => handleChange(e)}
+            >
+               {expenseTypesList.map(expense => (
+                  <SelectItem key={expense.value} value={expense.value}>
+                     {expense.label}
+                  </SelectItem>
+               ))}
+            </Select>
+            <Select
+               label="Payment type"
+               name="payment_type"
+               placeholder="Select payment type"
+               selectedKeys={[`${formData.payment_type}`]}
+               disabledKeys={['']}
+               isRequired
+               onChange={e => handleChange(e)}
+            >
+               <SelectItem key="" value={''}>
+                  Select type
+               </SelectItem>
+               <SelectItem key="single" value={'single'}>
+                  Single
+               </SelectItem>
+               <SelectItem key="monthly" value={'monthly'}>
+                  Monthly
+               </SelectItem>
+            </Select>
+            <Textarea
+               label="Description"
+               placeholder="(optional)"
+               name="description"
+               value={formData.description}
+               onChange={e => handleChange(e)}
+            />
+            <FormButton type="submit" color="primary">
+               Create Expense
+            </FormButton>
+         </form>
+      </div>
+   );
+};
