96ff4ce813a9440d1f047a033a58b263eebad56b
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-04-14 20:05:50 +0200

    fix(get-expenses): fixed issue with vercel postgres
    Vercel postgres db returns numeric data types as string because of that, sorting numeric data type values isn't possible as a result pagination and sorting by numeric/number values can't be performed correctly. Dropped idea of server side pagination and just added client side(it took me 4 days of trying to fix that but vercel postgres won't allow that... eh...)

--- src/lib/actions/expenses/get-expenses.ts
+++ src/lib/actions/expenses/get-expenses.ts
@@ -2,30 +2,23 @@
 import { sql } from '@vercel/postgres';
 import type { Expense } from '@lib/definitions';
 
-export async function getExpenses(user_id: string, page: number) {
-   // change this value to limit returned results from query
-   // as default 20
-   // to make it dynamic just pass prop i.e. 'resultsPerPage'
-   const resultsLimit = 20;
+type QueryProps = {
+   user_id: string;
+};
 
+export async function getExpenses({ user_id }: QueryProps) {
    try {
       const userExpenses = await sql<Expense>`
       SELECT id, name, amount, expense_type, payment_type, description, created_at, updated_at 
       FROM expenses 
       WHERE user_id=${user_id}
-      LIMIT ${resultsLimit}
-      OFFSET ${(page - 1) * resultsLimit}
       `;
 
-      const pagesInDB = await sql`
+      const totalResults = await sql`
       SELECT COUNT(*) AS total_pages
       FROM expenses
       WHERE user_id = ${user_id}
       `;
-
-      const totalPages = Math.ceil(
-         pagesInDB.rows[0].total_pages / resultsLimit
-      );
 
       if (userExpenses.rows.length === 0) {
          return { message: `You have no expenses to keep track of` };
@@ -33,7 +26,7 @@ export async function getExpenses(user_id: string, page: number) {
 
       return {
          data: userExpenses.rows,
-         totalPages: totalPages,
+         totalResults: totalResults.rowCount,
          status: 'success',
       };
    } catch (error) {
