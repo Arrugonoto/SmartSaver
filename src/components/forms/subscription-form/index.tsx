63f992322a0ac43479c6333f03e0d327b6875ecd
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-04-16 19:34:44 +0200

    refactor: refactor

--- src/components/forms/subscription-form/index.tsx
+++ src/components/forms/subscription-form/index.tsx
@@ -10,108 +10,106 @@ import { Select, SelectItem } from '@nextui-org/select';
 import { Divider } from '@nextui-org/divider';
 
 export const SubscriptionForm = ({ user_id }: { user_id: string }) => {
-   const [formData, setFormData] = useState<Omit<Expense, 'id' | 'created_at'>>(
-      {
-         user_id: user_id,
-         name: '',
-         amount: 0,
-         expense_type: '',
-         payment_type: 'subscription',
-      }
-   );
+  const [formData, setFormData] = useState<Omit<Expense, 'id' | 'created_at'>>({
+    user_id: user_id,
+    name: '',
+    amount: 0,
+    expense_type: '',
+    payment_type: 'subscription',
+  });
 
-   const resetForm = () => {
-      setFormData(prev => ({
-         ...prev,
-         name: '',
-         amount: 0,
-         expense_type: '',
-      }));
-   };
+  const resetForm = () => {
+    setFormData((prev) => ({
+      ...prev,
+      name: '',
+      amount: 0,
+      expense_type: '',
+    }));
+  };
 
-   const handleChange = (
-      e:
-         | React.ChangeEvent<HTMLSelectElement>
-         | React.ChangeEvent<HTMLInputElement>
-   ) => {
-      setFormData(prev => ({
-         ...prev,
-         [e.target.name]: e.target.value,
-      }));
-   };
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLSelectElement>
+      | React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-      // sometime TS is a conversion hell, knows that 'amount' should be number but,
-      // select makes it a string... I'm converting 'string to string' to be able to convert it to float...
-      const { amount } = formData;
-      const stringAmount = amount.toString();
-      const expenseAmount = parseFloat(stringAmount);
-      const expenseData = {
-         ...formData,
-         amount: expenseAmount,
-      };
-      console.log(expenseData);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // sometime TS is a conversion hell, knows that 'amount' should be number but,
+    // select makes it a string... I'm converting 'string to string' to be able to convert it to float...
+    const { amount } = formData;
+    const stringAmount = amount.toString();
+    const expenseAmount = parseFloat(stringAmount);
+    const expenseData = {
+      ...formData,
+      amount: expenseAmount,
+    };
+    console.log(expenseData);
 
-      await createExpense(expenseData);
-      resetForm();
-   };
+    await createExpense(expenseData);
+    resetForm();
+  };
 
-   return (
-      <div className="w-full">
-         <Accordion variant="shadow">
-            <AccordionItem
-               key="expense-subscription"
-               aria-label="Accordion subscription form"
-               title="Fill form or select from list"
+  return (
+    <div className="w-full">
+      <Accordion variant="shadow">
+        <AccordionItem
+          key="expense-subscription"
+          aria-label="Accordion subscription form"
+          title="Fill form or select from list"
+        >
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-3 w-full pb-4"
+          >
+            <Input
+              type="text"
+              name="name"
+              label="Subscription name"
+              value={formData.name}
+              isRequired
+              onChange={(e) => handleChange(e)}
+            />
+            <Input
+              type="number"
+              name="amount"
+              label="Subscription amount"
+              value={formData.amount.toString()}
+              step="0.01"
+              min={1}
+              isRequired
+              onChange={(e) => handleChange(e)}
+            />
+            <Select
+              label="Expense type"
+              name="expense_type"
+              placeholder="Select expense type"
+              selectedKeys={[`${formData.expense_type}`]}
+              disabledKeys={['']}
+              isRequired
+              onChange={(e) => handleChange(e)}
             >
-               <form
-                  onSubmit={handleSubmit}
-                  className="flex flex-col gap-3 w-full pb-4"
-               >
-                  <Input
-                     type="text"
-                     name="name"
-                     label="Subscription name"
-                     value={formData.name}
-                     isRequired
-                     onChange={e => handleChange(e)}
-                  />
-                  <Input
-                     type="number"
-                     name="amount"
-                     label="Subscription amount"
-                     value={formData.amount.toString()}
-                     step="0.01"
-                     min={1}
-                     isRequired
-                     onChange={e => handleChange(e)}
-                  />
-                  <Select
-                     label="Expense type"
-                     name="expense_type"
-                     placeholder="Select expense type"
-                     selectedKeys={[`${formData.expense_type}`]}
-                     disabledKeys={['']}
-                     isRequired
-                     onChange={e => handleChange(e)}
-                  >
-                     {expenseTypesList.map(expense => (
-                        <SelectItem key={expense.value} value={expense.value}>
-                           {expense.label}
-                        </SelectItem>
-                     ))}
-                  </Select>
-                  <FormButton type="submit" color="primary">
-                     Add Subscription
-                  </FormButton>
-               </form>
-            </AccordionItem>
-         </Accordion>
-         <Divider className="my-4" />
-         <div>
-            <p className="text-center">List of subscriptions</p>
-         </div>
+              {expenseTypesList.map((expense) => (
+                <SelectItem key={expense.value} value={expense.value}>
+                  {expense.label}
+                </SelectItem>
+              ))}
+            </Select>
+            <FormButton type="submit" color="primary">
+              Add Subscription
+            </FormButton>
+          </form>
+        </AccordionItem>
+      </Accordion>
+      <Divider className="my-4" />
+      <div>
+        <p className="text-center">List of popular subscriptions</p>
       </div>
-   );
+    </div>
+  );
 };
--- src/components/modals/create-expense-modal/index.tsx
+++ src/components/modals/create-expense-modal/index.tsx
@@ -1,10 +1,11 @@
 'use client';
 import {
-   Modal,
-   ModalContent,
-   ModalHeader,
-   ModalBody,
-   useDisclosure,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
 } from '@nextui-org/modal';
 import { Button } from '@nextui-org/button';
 import { Tabs, Tab } from '@nextui-org/tabs';
@@ -12,41 +13,43 @@ import { ExpensesForm } from '@/components/forms/expenses-form';
 import { SubscriptionForm } from '@components/forms/subscription-form';
 
 export const CreateExpenseModal = ({ user_id }: { user_id: string }) => {
-   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
-   return (
-      <div>
-         <Button onPress={onOpen} color="primary">
-            Add new expense
-         </Button>
-         <Modal
-            isOpen={isOpen}
-            onOpenChange={onOpenChange}
-            className="min-h-[30rem]"
-            size="xl"
-            isDismissable={false}
-            isKeyboardDismissDisabled={false}
-         >
-            <ModalContent className="">
-               {onClose => (
-                  <>
-                     <ModalHeader className="flex flex-col gap-1">
-                        Add New Expense
-                     </ModalHeader>
-                     <ModalBody>
-                        <Tabs aria-label="Options" className="justify-center">
-                           <Tab key="standard" title="Standard">
-                              <ExpensesForm user_id={user_id} />
-                           </Tab>
-                           <Tab key="subscription" title="Subscription">
-                              <SubscriptionForm user_id={user_id} />
-                           </Tab>
-                        </Tabs>
-                     </ModalBody>
-                  </>
-               )}
-            </ModalContent>
-         </Modal>
-      </div>
-   );
+  return (
+    <div>
+      <Button onPress={onOpen} color="primary">
+        Add new expense
+      </Button>
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        className="h-[34rem]"
+        size="xl"
+        isDismissable={false}
+        isKeyboardDismissDisabled={false}
+        scrollBehavior={'inside'}
+      >
+        <ModalContent className="">
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Add New Expense
+              </ModalHeader>
+              <ModalBody>
+                <Tabs aria-label="Options" className="justify-center">
+                  <Tab key="standard" title="Standard">
+                    <ExpensesForm user_id={user_id} />
+                  </Tab>
+                  <Tab key="subscription" title="Subscription">
+                    <SubscriptionForm user_id={user_id} />
+                  </Tab>
+                </Tabs>
+              </ModalBody>
+              <ModalFooter></ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </div>
+  );
 };
