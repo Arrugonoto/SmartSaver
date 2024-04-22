bf55c75ae532a184180c9b21104fa5a60d18ac0c
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-04-22 22:11:55 +0200

    refactor(get-expenses)

--- src/lib/actions/expenses/get-expenses.ts
+++ src/lib/actions/expenses/get-expenses.ts
@@ -2,36 +2,32 @@
 import { sql } from '@vercel/postgres';
 import type { Expense } from '@lib/definitions';
 
-type QueryProps = {
-   user_id: string;
-};
-
-export async function getExpenses({ user_id }: QueryProps) {
-   try {
-      const userExpenses = await sql<Expense>`
+export async function getExpenses(user_id: string) {
+  try {
+    const userExpenses = await sql<Expense>`
       SELECT id, name, amount, expense_type, payment_type, description, created_at, updated_at 
       FROM expenses 
       WHERE user_id=${user_id}
       `;
 
-      const totalResults = await sql`
+    const totalResults = await sql`
       SELECT COUNT(*) AS total_pages
       FROM expenses
       WHERE user_id = ${user_id}
       `;
 
-      if (userExpenses.rows.length === 0) {
-         return { message: `You have no expenses to keep track of` };
-      }
+    if (userExpenses.rows.length === 0) {
+      return { message: `You have no expenses to keep track of` };
+    }
 
-      return {
-         data: userExpenses.rows,
-         totalResults: totalResults.rowCount,
-         status: 'success',
-      };
-   } catch (error) {
-      const errorMessage = error instanceof Error && error.message;
-      console.log(error);
-      return { error: error, message: errorMessage, status: 'fail' };
-   }
+    return {
+      data: userExpenses.rows,
+      totalResults: totalResults.rowCount,
+      status: 'success',
+    };
+  } catch (error) {
+    const errorMessage = error instanceof Error && error.message;
+    console.log(error);
+    return { error: error, message: errorMessage, status: 'fail' };
+  }
 }
