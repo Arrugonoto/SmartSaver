13eb778536568d7081e1282f8d37599403823f35
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-04-23 20:00:10 +0200

    udpate(dashboard page): extracted session and passed it to necessary component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,19 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@app/api/auth/[...nextauth]/options';
 import { Metadata } from 'next';
+import { OverviewSection } from '@components/sections/overview-section';
 
 export const metadata: Metadata = {
-   title: 'Overview',
+  title: 'Overview',
 };
 
-export default function Dashboard() {
-   return <main className="flex flex-1 flex-col">Dashboard Page</main>;
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+  const user_id = session?.user.id;
+
+  return (
+    <main className="flex flex-1 flex-col">
+      <OverviewSection user_id={user_id} />
+    </main>
+  );
 }
