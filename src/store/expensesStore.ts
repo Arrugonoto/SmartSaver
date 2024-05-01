57d67449fa759ca1275d70ac7521075f34647609
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-05-01 19:06:39 +0200

    refactor(expensesStore): dropped approach with initial fetch on subscribe

--- src/store/expensesStore.ts
+++ src/store/expensesStore.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 import type { Expense } from '@lib/constants/types/expenses/expenses';
 
 type State = {
+  userId: string;
   expenses: Expense[];
   page: number;
   resultsPerPage: number;
@@ -9,19 +10,22 @@ type State = {
 };
 
 type Action = {
+  setUserId: (userId: State['userId']) => void;
   setExpenses: (expenses: State['expenses']) => void;
   setPage: (page: State['page']) => void;
   setResultsPerPage: (resultsPerPage: State['resultsPerPage']) => void;
   setTotalResults: (totalResults: State['totalResults']) => void;
 };
 
-export const useExpensesStore = create<State & Action>((set) => ({
+export const useExpensesStore = create<State & Action>((set, get) => ({
+  userId: '',
   expenses: [],
   page: 1,
   resultsPerPage: 20,
   totalResults: 0,
+  setUserId: (id) => set({ userId: id }),
   setExpenses: (newExpenses) => set({ expenses: newExpenses }),
-  setPage: (pageNum) => set(() => ({ page: pageNum })),
+  setPage: (pageNum) => set({ page: pageNum }),
   setResultsPerPage: (resPerPage) =>
     set(() => ({ resultsPerPage: resPerPage })),
   setTotalResults: (numOfTotal) => set({ totalResults: numOfTotal }),
