a309fee1a9b568a349b9a39758d692694d430f75
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-05-03 19:11:33 +0200

    update(expensesStore): added Zustand 'persist' method to persist data between renders/page refresh

--- src/store/expensesStore.ts
+++ src/store/expensesStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 import type { Expense } from '@lib/constants/types/expenses/expenses';
 
 type State = {
@@ -17,16 +18,24 @@ type Action = {
   setTotalResults: (totalResults: State['totalResults']) => void;
 };
 
-export const useExpensesStore = create<State & Action>((set, get) => ({
-  userId: '',
-  expenses: [],
-  page: 1,
-  resultsPerPage: 20,
-  totalResults: 0,
-  setUserId: (id) => set({ userId: id }),
-  setExpenses: (newExpenses) => set({ expenses: newExpenses }),
-  setPage: (pageNum) => set({ page: pageNum }),
-  setResultsPerPage: (resPerPage) =>
-    set(() => ({ resultsPerPage: resPerPage })),
-  setTotalResults: (numOfTotal) => set({ totalResults: numOfTotal }),
-}));
+export const useExpensesStore = create(
+  persist<State & Action>(
+    (set, get) => ({
+      userId: '',
+      expenses: [],
+      page: 1,
+      resultsPerPage: 20,
+      totalResults: 0,
+      setUserId: (id) => set({ userId: id }),
+      setExpenses: (newExpenses) => set({ expenses: newExpenses }),
+      setPage: (pageNum) => set({ page: pageNum }),
+      setResultsPerPage: (resPerPage) =>
+        set(() => ({ resultsPerPage: resPerPage })),
+      setTotalResults: (numOfTotal) => set({ totalResults: numOfTotal }),
+    }),
+    {
+      name: 'expenses-storage',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
