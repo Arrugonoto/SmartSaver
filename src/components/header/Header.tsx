13eb0306e834453b77992723702fde9c2203d51f
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-05-05 18:34:11 +0200

    fix(Header): fixed issue with z position overflow

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,38 +1,38 @@
 'use client';
 import {
-   Navbar,
-   NavbarBrand,
-   NavbarContent,
-   NavbarItem,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
 } from '@nextui-org/navbar';
 import { Button } from '@nextui-org/button';
 import Link from 'next/link';
 import { signOut, useSession } from 'next-auth/react';
 
 export const Header = () => {
-   const { data: session } = useSession();
+  const { data: session } = useSession();
 
-   return (
-      <header className="sticky top-0 left-0 w-full ">
-         <Navbar className="flex" isBordered shouldHideOnScroll maxWidth="full">
-            <NavbarBrand>im a header</NavbarBrand>
-            <NavbarContent justify="end">
-               <NavbarItem>
-                  <p>{session && `Hi ${session?.user?.name}`}</p>
-               </NavbarItem>
-               <NavbarItem>
-                  {session ? (
-                     <Button onPress={() => signOut({ callbackUrl: '/' })}>
-                        Logout
-                     </Button>
-                  ) : (
-                     <Link href="/login">
-                        <Button>Sign In</Button>
-                     </Link>
-                  )}
-               </NavbarItem>
-            </NavbarContent>
-         </Navbar>
-      </header>
-   );
+  return (
+    <header className="sticky top-0 left-0 w-full z-[999]">
+      <Navbar className="flex" isBordered shouldHideOnScroll maxWidth="full">
+        <NavbarBrand>im a header</NavbarBrand>
+        <NavbarContent justify="end">
+          <NavbarItem>
+            <p>{session && `Hi ${session?.user?.name}`}</p>
+          </NavbarItem>
+          <NavbarItem>
+            {session ? (
+              <Button onPress={() => signOut({ callbackUrl: '/' })}>
+                Logout
+              </Button>
+            ) : (
+              <Link href="/login">
+                <Button>Sign In</Button>
+              </Link>
+            )}
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
+    </header>
+  );
 };
