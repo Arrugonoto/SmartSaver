a480026f60971be89ec158eda650ccb52b02d2ed
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-05-07 11:29:06 +0200

    update: added color for each category to improve visualization on chart

--- src/lib/constants/data/dummy/expense-categories.ts
+++ src/lib/constants/data/dummy/expense-categories.ts
@@ -1,49 +1,60 @@
 import type { ExpenseCategories } from '@constants/types/expenses/expenses';
 
 export const expenseCategoriesList: ExpenseCategories[] = [
-  { label: 'Select type', value: '' },
+  { label: 'Select type', value: '', color: '' },
   {
     label: 'Basic',
     value: 'basic-expenses',
+    color: 'ff6200',
   },
   {
     label: 'Beauty',
     value: 'beauty',
+    color: 'ab0066',
   },
   {
     label: 'Bills',
     value: 'bills',
+    color: '525199',
   },
   {
     label: 'Car & Transport',
     value: 'car-and-transport',
+    color: '9d64b5',
   },
   {
     label: 'Clothing & Footwear',
     value: 'clothing-and-footwear',
+    color: '559bd1',
   },
   {
     label: 'Education',
     value: 'education',
+    color: '008e91',
   },
   {
     label: 'Entertainment',
     value: 'entertainment',
+    color: 'c66152',
   },
   {
     label: 'Healthcare',
     value: 'healthcare',
+    color: '349651',
   },
   {
     label: 'Self Improvement',
     value: 'self-improvement',
+    color: 'fffd18',
   },
   {
     label: 'Travel',
     value: 'travel',
+    color: 'c68c52',
   },
   {
     label: 'Other',
     value: 'other',
+    color: 'd3d3d3',
   },
 ];
