16101d0c12b5a40a81ce872781c255e7e579c6de
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-05-14 23:59:28 +0200

    feat(annual-spendings-chart): added possibility to select year of tracked data(based on provided data)

--- src/components/charts/annual-spendings-area-chart/index.tsx
+++ src/components/charts/annual-spendings-area-chart/index.tsx
@@ -11,6 +11,7 @@ import {
 } from 'recharts';
 import { useExpensesStore } from '@store/expensesStore';
 import { useStore } from '@lib/hooks/useStore';
+import { Select, SelectItem } from '@nextui-org/select';
 
 type AnnualChartElement = {
   name: string;
@@ -32,11 +33,15 @@ const months = [
   { name: 'December', abbreviation: 'Dec', numeric: '12' },
 ];
 
-const formatChartData = (data: any[]) => {
+const formatChartData = (data: any[], year: string) => {
+  const filteredByYear = data.filter((expense) =>
+    expense.created_at.toString().includes(year)
+  );
+
   const chartData = months.map((month) => {
     // filter spendings for each month separately
     // and filter empty values(undefined)
-    const eachMonthSpendings = data
+    const eachMonthSpendings = filteredByYear
       ?.map((expense) => {
         if (expense.created_at.toString().includes(month.abbreviation))
           return expense.amount;
@@ -59,59 +64,77 @@ const formatChartData = (data: any[]) => {
 };
 
 const getYears = (data: any[]) => {
-  const years = data.map((expense) => expense.created_at.getFullYear());
-
+  const years = data?.map((expense) => {
+    const createdAt = new Date(expense.created_at);
+    return createdAt.getFullYear().toString();
+  });
   const uniqYears = [...new Set(years)];
+  console.log(uniqYears);
   return uniqYears as string[];
 };
 
 export const AnnualSpendingsAreaChart = () => {
   const expenses = useStore(useExpensesStore, (state) => state.expenses);
   const [chartData, setChartData] = useState<AnnualChartElement[]>([]);
-  const [dataYears, setDataYears] = useState<string[]>([]);
+  const [yearsOfData, setYearsOfData] = useState<string[]>([]);
+  const [filteredYear, setFilteredYear] = useState<string>('2024');
 
   useEffect(() => {
     if (expenses) {
-      const data = formatChartData(expenses);
       const years = getYears(expenses);
-      setChartData(data);
-      setDataYears(years);
+      setYearsOfData(years);
     }
   }, [expenses]);
-  console.log(dataYears);
+
+  useEffect(() => {
+    if (expenses) {
+      const data = formatChartData(expenses, filteredYear);
+
+      setChartData(data);
+    }
+  }, [expenses, filteredYear]);
 
   return (
-    <div className="h-[400px] w-full">
-      <ResponsiveContainer width="100%" height="100%">
-        <AreaChart
-          width={500}
-          height={400}
-          data={chartData}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip
-            labelStyle={{ color: '#000' }}
-            contentStyle={{
-              borderRadius: '0.3rem',
+    <div className="w-full">
+      <Select label="Select an animal" className="max-w-xs">
+        {yearsOfData.map((year, index) => (
+          <SelectItem key={index} value={year}>
+            {year}
+          </SelectItem>
+        ))}
+      </Select>
+      <div className="h-[400px] w-full">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={chartData}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
             }}
-          />
-          <Legend />
-          <Area
-            type="bumpX"
-            name="Total in month"
-            dataKey="spendings"
-            fill="#3182bd"
-            fillOpacity={0.2}
-          />
-        </AreaChart>
-      </ResponsiveContainer>
+          >
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip
+              labelStyle={{ color: '#000' }}
+              contentStyle={{
+                borderRadius: '0.3rem',
+              }}
+            />
+            <Legend />
+            <Area
+              type="bumpX"
+              name="Total in month"
+              dataKey="spendings"
+              fill="#3182bd"
+              fillOpacity={0.2}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 };
