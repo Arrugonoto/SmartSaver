87d7147e71bbae1bbc95272080accf9ee0f7839b
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-05-17 20:49:59 +0200

    refactor(overview-section): refactor(improved readability)

--- src/components/sections/overview-section/index.tsx
+++ src/components/sections/overview-section/index.tsx
@@ -7,17 +7,12 @@ import { useStore } from '@lib/hooks/useStore';
 import { useExpensesStore } from '@store/expensesStore';
 import { Card, CardBody } from '@nextui-org/card';
 import { Tabs, Tab } from '@nextui-org/tabs';
-import {
-  Table,
-  TableHeader,
-  TableColumn,
-  TableBody,
-  TableRow,
-  TableCell,
-  getKeyValue,
-} from '@nextui-org/table';
-import { AnnualSpendingsBarChart } from '@components/charts/annual-spendings-bar-chart';
+import { AnnualSpendingsAreaChart } from '@components/charts/annual-spendings-area-chart';
 import { ExpenseCategoryPieChart } from '@components/charts/category-pie-chart';
+import { ExpenseCategoryBarChart } from '@components/charts/category-bar-chart';
+import { Select, SelectItem } from '@nextui-org/select';
+import { selectIcons } from '@constants/icons';
+import { TopSpendingsTabs } from '@components/tabs/top-spendings';
 
 export const OverviewSection = ({ user_id }: { user_id: string }) => {
   const setExpenses = useExpensesStore((state) => state.setExpenses);
@@ -28,36 +23,17 @@ export const OverviewSection = ({ user_id }: { user_id: string }) => {
   });
   const expenses = useStore(useExpensesStore, (state) => state.expenses);
 
-  const columns = [
-    {
-      key: 'name',
-      label: 'NAME',
-    },
-    {
-      key: 'amount',
-      label: 'AMOUNT',
-    },
-  ];
-
   const totalExpenses = expenses?.reduce(
     (sum, expense) => sum + parseFloat(expense.amount as any),
     0
   );
 
-  const sortedExpenses = expenses
-    ?.sort(
-      (curr, next) =>
-        parseFloat(next.amount as any) - parseFloat(curr.amount as any)
-    )
-    .slice(0, 10);
-
-  const highestOneTimePayments = expenses
-    ?.filter((expense) => expense.payment_type === 'one-time')
-    .sort(
-      (curr, next) =>
-        parseFloat(next.amount as any) - parseFloat(curr.amount as any)
-    )
-    .slice(0, 10);
+  const totalNumOfExpensesInMonth = expenses?.filter(
+    (expense) =>
+      expense.created_at.toString().includes('Apr') ||
+      expense.payment_type.toLocaleLowerCase().includes('monthly') ||
+      expense.payment_type.toLowerCase().includes('subscription')
+  ).length;
 
   const highestSubscriptions = expenses
     ?.filter((expense) => expense.payment_type === 'subscription')
@@ -88,90 +64,68 @@ export const OverviewSection = ({ user_id }: { user_id: string }) => {
   return (
     <div className="flex flex-col w-full h-full p-2 gap-4">
       <h1 className="text-2xl mb-4">Spendings overview</h1>
-      <section className="flex flex-col w-full">
-        <div className="flex gap-4">
-          <Card className="max-w-[300px] w-full align-center justify-center">
-            <CardBody className="gap-4">
-              <h2 className="text-center text-xl">Total spendings</h2>
-              <p className="text-center text-2xl">{totalExpenses}</p>
-            </CardBody>
-          </Card>
-          <Card className="max-w-[300px] w-full align-center justify-center">
-            <CardBody className="gap-4">
-              <h2 className="text-center text-xl">Monthly commitments</h2>
-              <p className="text-center text-2xl">{monthlyCommitments()}</p>
+      <section className="flex w-full gap-4">
+        <div className="flex flex-col max-w-[620px] w-full gap-2">
+          <div className="flex gap-2 w-full">
+            <Card className="w-full align-center justify-center">
+              <CardBody className="w-full gap-4">
+                <h2 className="text-center text-xl">Total spendings</h2>
+                <p className="text-center text-2xl">{totalExpenses}</p>
+              </CardBody>
+            </Card>
+            <Card className="w-full align-center justify-center">
+              <CardBody className="gap-4">
+                <h2 className="text-center text-xl">Monthly commitments</h2>
+                <p className="text-center text-2xl">{monthlyCommitments()}</p>
+              </CardBody>
+            </Card>
+          </div>
+          <Card className="flex items-center min-h-[320px] w-full p-4 gap-2">
+            <h2 className="text-xl">10 most expensive spendings</h2>
+            <CardBody>
+              <TopSpendingsTabs />
             </CardBody>
           </Card>
         </div>
-      </section>
-      <section className="">
-        <Card className="flex items-center max-w-[620px] min-h-[320px] p-4 gap-2">
-          <h2 className="text-xl">10 most expensive fees</h2>
-          <Tabs aria-label="Options" className="w-auto" size="sm" radius="lg">
-            <Tab key="overall" title="Overall" className="w-full">
-              <Table aria-label="10 highest expenses">
-                <TableHeader columns={columns}>
-                  {(column) => (
-                    <TableColumn key={column.key}>{column.label}</TableColumn>
-                  )}
-                </TableHeader>
-                <TableBody items={sortedExpenses}>
-                  {(item) => (
-                    <TableRow key={item.id}>
-                      {(columnKey) => (
-                        <TableCell>{getKeyValue(item, columnKey)}</TableCell>
-                      )}
-                    </TableRow>
-                  )}
-                </TableBody>
-              </Table>
-            </Tab>
-            <Tab key="one time" title="One Time" className="w-full">
-              <Table aria-label="10 highest one time payments">
-                <TableHeader columns={columns}>
-                  {(column) => (
-                    <TableColumn key={column.key}>{column.label}</TableColumn>
-                  )}
-                </TableHeader>
-                <TableBody items={highestOneTimePayments}>
-                  {(item) => (
-                    <TableRow key={item.id}>
-                      {(columnKey) => (
-                        <TableCell>{getKeyValue(item, columnKey)}</TableCell>
-                      )}
-                    </TableRow>
-                  )}
-                </TableBody>
-              </Table>
-            </Tab>
-            <Tab key="subscriptions" title="Subscriptions" className="w-full">
-              <Table aria-label="10 highest subscriptions">
-                <TableHeader columns={columns}>
-                  {(column) => (
-                    <TableColumn key={column.key}>{column.label}</TableColumn>
-                  )}
-                </TableHeader>
-                <TableBody items={highestSubscriptions}>
-                  {(item) => (
-                    <TableRow key={item.id}>
-                      {(columnKey) => (
-                        <TableCell>{getKeyValue(item, columnKey)}</TableCell>
-                      )}
-                    </TableRow>
-                  )}
-                </TableBody>
-              </Table>
-            </Tab>
-          </Tabs>
-        </Card>
-      </section>
-      <section>
-        <Card className="w-full p-4">
-          <AnnualSpendingsBarChart />
-        </Card>
         <div>spendings bar chart by current and previous month</div>
+      </section>
+      <section className="flex flex-col pb-8 pr-2 xl:pr-4 gap-4">
         <Card className="w-full p-4">
-          <ExpenseCategoryPieChart />
+          <AnnualSpendingsAreaChart />
+        </Card>
+
+        <Card className="flex flex-col w-full px-4 pt-4 gap-4">
+          <div className="flex w-full justify-between">
+            <p>Total number of expenses: {totalNumOfExpensesInMonth}</p>
+            <Select
+              label="Select date range"
+              variant="bordered"
+              defaultSelectedKeys={['current_month']}
+              size="md"
+              className="max-w-xs"
+              startContent={<selectIcons.calendarEmpty />}
+            >
+              <SelectItem key={'current_month'} value={'current_month'}>
+                Current month
+              </SelectItem>
+              <SelectItem key={'previous_month'} value={'previous_month'}>
+                Last month
+              </SelectItem>
+              <SelectItem key={'last_three_months'} value={'last_three_months'}>
+                Last 3 months
+              </SelectItem>
+              <SelectItem key={'last_six_months'} value={'last_six_months'}>
+                Last 6 months
+              </SelectItem>
+              <SelectItem key={'last_year'} value={'last_year'}>
+                Last 12 months
+              </SelectItem>
+            </Select>
+          </div>
+          <div className="flex flex-col w-full min-h-[800px] lg:flex-row lg:min-h-[440px] p-4">
+            <ExpenseCategoryPieChart />
+            <ExpenseCategoryBarChart />
+          </div>
         </Card>
       </section>
     </div>
