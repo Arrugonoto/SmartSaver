1c826ffcc2dc2651d1882e228205d815d0d4fa43
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-05-21 01:10:08 +0200

    rearrange(months): added dummy data for months

---
+++ src/lib/constants/data/dummy/months.ts
@@ -0,0 +1,20 @@
+type MonthType = {
+  name: string;
+  abbreviation: string;
+  numeric: string;
+};
+
+export const months: MonthType[] = [
+  { name: 'January', abbreviation: 'Jan', numeric: '01' },
+  { name: 'February', abbreviation: 'Feb', numeric: '02' },
+  { name: 'March', abbreviation: 'Mar', numeric: '03' },
+  { name: 'April', abbreviation: 'Apr', numeric: '04' },
+  { name: 'May', abbreviation: 'May', numeric: '05' },
+  { name: 'June', abbreviation: 'June', numeric: '06' },
+  { name: 'July', abbreviation: 'July', numeric: '07' },
+  { name: 'August', abbreviation: 'Aug', numeric: '08' },
+  { name: 'September', abbreviation: 'Sept', numeric: '09' },
+  { name: 'October', abbreviation: 'Oct', numeric: '10' },
+  { name: 'November', abbreviation: 'Nov', numeric: '11' },
+  { name: 'December', abbreviation: 'Dec', numeric: '12' },
+];
