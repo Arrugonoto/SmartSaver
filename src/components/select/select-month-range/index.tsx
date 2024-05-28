4a2abcd37bb03050ca3c5b9121a5d5b1fa7abe63
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-05-28 20:27:20 +0200

    rearrange/feat(select-month-range): added select component for choosing monthly date range of charts data

---
+++ src/components/select/select-month-range/index.tsx
@@ -0,0 +1,52 @@
+'use client';
+import { Select, SelectItem } from '@nextui-org/select';
+import { selectIcons } from '@constants/icons';
+
+type DateRange = {
+  key: string;
+  name: string;
+  value: string;
+};
+
+const datesRange: DateRange[] = [
+  { key: 'current_month', name: 'Current month', value: 'current_month' },
+  { key: 'last_month', name: 'Last month', value: 'last_month' },
+  {
+    key: 'last_three_months',
+    name: 'Last 3 months',
+    value: 'last_three_months',
+  },
+  { key: 'last_six_months', name: 'Last 6 months', value: 'last_six_months' },
+  { key: 'last_year', name: 'Last 12 months', value: 'last_year' },
+];
+
+export const MonthRangeSelect = ({
+  dateRange,
+  setDateRange,
+}: {
+  dateRange: string;
+  setDateRange: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setDateRange(e.target.value);
+  };
+
+  return (
+    <Select
+      label="Select date range"
+      variant="bordered"
+      selectedKeys={[dateRange]}
+      disallowEmptySelection={true}
+      size="md"
+      className="max-w-xs"
+      startContent={<selectIcons.calendarEmpty />}
+      onChange={(e) => handleChange(e)}
+    >
+      {datesRange.map((el) => (
+        <SelectItem key={el.key} value={el.value}>
+          {el.name}
+        </SelectItem>
+      ))}
+    </Select>
+  );
+};
