44d0b0420b13f5d461dadeb0a27b6d9a0a1f7a37
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-05-28 18:40:33 +0200

    refactor(overview): removed unused code

--- src/components/overview/index.tsx
+++ src/components/overview/index.tsx
@@ -1,48 +1,20 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import type { Expense } from '@constants/types/expenses/expenses';
 import { getExpenses } from '@lib/actions/expenses/get-expenses';
 import { useFetch } from '@lib/hooks/useFetch';
-import { useStore } from '@lib/hooks/useStore';
 import { useExpensesStore } from '@store/expensesStore';
 import { ExpensesSummarySection } from '@components/sections/overview/summary';
 import { MonthlyChartsSection } from '@components/sections/overview/monthly-charts';
 import { AnnualChartSection } from '@components/sections/overview/annual-chart';
 
-const filterByDateRange = (data: Expense[], monthsRange: number) => {
-  // function for filtering data based on range of dates
-  const currentDate = new Date();
-  const lastDayOfMonth = new Date(
-    currentDate.getFullYear(),
-    currentDate.getMonth() + 1,
-    0
-  );
-  // get first day of first month in range
-  const startDate = new Date(
-    currentDate.getFullYear(),
-    currentDate.getMonth() - monthsRange + 1,
-    1
-  );
-
-  const filteredData = data.filter((expense) => {
-    const expenseDate = new Date(expense.created_at);
-    // check if searched element is inside specified date range
-    return expenseDate >= startDate && expenseDate <= lastDayOfMonth;
-  });
-
-  return filteredData;
-};
-
 export const ExpensesOverview = ({ user_id }: { user_id: string }) => {
-  const [dataByDates, setDataByDates] = useState<Expense[]>([]);
-  const [dateRange, setDateRange] = useState<string>('current_month');
   const setExpenses = useExpensesStore((state) => state.setExpenses);
   const setTotalResults = useExpensesStore((state) => state.setTotalResults);
   const { data, totalResults } = useFetch<Expense>({
     action: getExpenses,
     user_id,
   });
-  const expenses = useStore(useExpensesStore, (state) => state.expenses);
 
   useEffect(() => {
     setExpenses(data);
@@ -51,13 +23,6 @@ export const ExpensesOverview = ({ user_id }: { user_id: string }) => {
   useEffect(() => {
     setTotalResults(totalResults);
   }, [totalResults, setTotalResults]);
-
-  useEffect(() => {
-    if (expenses) {
-      const dateRangeData = filterByDateRange(expenses, 1);
-      setDataByDates(dateRangeData);
-    }
-  }, [expenses]);
 
   return (
     <div className="flex flex-col w-full h-full p-2 gap-4 xl:pr-6">
