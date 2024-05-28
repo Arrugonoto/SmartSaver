5d3c708297c6c27c2d32476e0d7a6dc8ccbc20d4
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-05-28 20:28:59 +0200

    update(monthly-charts): moved date range select to separate file(component)

--- src/components/sections/overview/monthly-charts/index.tsx
+++ src/components/sections/overview/monthly-charts/index.tsx
@@ -6,8 +6,7 @@ import type { Expense } from '@constants/types/expenses/expenses';
 import { Card, CardHeader, CardBody } from '@nextui-org/card';
 import { ExpenseCategoryPieChart } from '@components/charts/category-pie-chart';
 import { ExpenseCategoryBarChart } from '@components/charts/category-bar-chart';
-import { Select, SelectItem } from '@nextui-org/select';
-import { selectIcons } from '@constants/icons';
+import { MonthRangeSelect } from '@components/select/select-month-range';
 
 const switchDateRange = (data: Expense[], dateRange: string) => {
   switch (dateRange) {
@@ -80,10 +79,6 @@ export const MonthlyChartsSection = () => {
 
   const totalNumOfExpensesInMonth = dataByDates.length;
 
-  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setDateRange(e.target.value);
-  };
-
   useEffect(() => {
     if (expenses) {
       const data = switchDateRange(expenses, dateRange);
@@ -98,32 +93,10 @@ export const MonthlyChartsSection = () => {
         <CardBody className="w-full">
           <div className="flex w-full justify-between">
             <p>Total number of expenses: {totalNumOfExpensesInMonth}</p>
-            <Select
-              label="Select date range"
-              variant="bordered"
-              defaultSelectedKeys={['current_month']}
-              disallowEmptySelection={true}
-              size="md"
-              className="max-w-xs"
-              startContent={<selectIcons.calendarEmpty />}
-              onChange={(e) => handleChange(e)}
-            >
-              <SelectItem key={'current_month'} value={'current_month'}>
-                Current month
-              </SelectItem>
-              <SelectItem key={'last_month'} value={'last_month'}>
-                Last month
-              </SelectItem>
-              <SelectItem key={'last_three_months'} value={'last_three_months'}>
-                Last 3 months
-              </SelectItem>
-              <SelectItem key={'last_six_months'} value={'last_six_months'}>
-                Last 6 months
-              </SelectItem>
-              <SelectItem key={'last_year'} value={'last_year'}>
-                Last 12 months
-              </SelectItem>
-            </Select>
+            <MonthRangeSelect
+              dateRange={dateRange}
+              setDateRange={setDateRange}
+            />
           </div>
           <div className="flex flex-col w-full min-h-[800px] lg:flex-row lg:min-h-[440px] p-4">
             <ExpenseCategoryPieChart expenses={dataByDates} />
