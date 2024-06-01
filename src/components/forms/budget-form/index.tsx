82d7bdd799a7e68c94429c237bcc205d677342ca
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-06-01 21:17:09 +0200

    update(budget-form): added variant 'update' to check if data should be set or updated

--- src/components/forms/budget-form/index.tsx
+++ src/components/forms/budget-form/index.tsx
@@ -2,13 +2,23 @@
 import React, { useState } from 'react';
 import { Input } from '@nextui-org/input';
 import { createBudgetLimit } from '@lib/actions/budget/create-budget-limit';
+import { updateBudgetLimit } from '@lib/actions/budget/update-budget-limit';
+import { getBudgetLimit } from '@lib/actions/budget/get-budget-limit';
 import { useSession } from 'next-auth/react';
 import FormButton from '@components/buttons/FormButton';
+import { useFetch } from '@lib/hooks/useFetch';
+import type { BudgetLimit } from '@lib/constants/types/budget/budget';
 
-export const BudgetForm = () => {
+export const BudgetForm = ({ update }: { update?: boolean }) => {
   const { data: session } = useSession();
   const user = session?.user;
+  const user_id = user?.id;
   const [budgetLimit, setBudgetLimit] = useState<number | null>(null);
+
+  const { data } = useFetch<BudgetLimit>({
+    action: getBudgetLimit,
+    user_id,
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const parsedLimit = parseFloat(e.target.value);
@@ -17,10 +27,13 @@ export const BudgetForm = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(budgetLimit);
 
-    if (user && budgetLimit) {
-      await createBudgetLimit({ user_id: user.id, budget_limit: budgetLimit });
+    if (!update && user && budgetLimit) {
+      await createBudgetLimit({ user_id, budget_limit: budgetLimit });
+    }
+
+    if (update && user && budgetLimit) {
+      updateBudgetLimit({ id: data?.id, budget_limit: budgetLimit });
     }
 
     resetForm();
@@ -37,15 +50,34 @@ export const BudgetForm = () => {
           type="number"
           name="budgetLimit"
           label="Budget limit"
+          placeholder={`${data?.budget_limit}$`}
           value={budgetLimit?.toString() || ''}
+          isInvalid={
+            update && budgetLimit !== null && data?.budget_limit == budgetLimit
+          }
           step="0.01"
           min={1}
           isRequired
+          errorMessage="New limit can't be same as current limit"
           onChange={(e) => handleChange(e)}
         />
-        <FormButton type="submit" color="primary">
-          Save limit
-        </FormButton>
+
+        {update ? (
+          <FormButton
+            type="submit"
+            color="primary"
+            isDisabled={
+              budgetLimit !== null &&
+              (data?.budget_limit as number) == budgetLimit
+            }
+          >
+            Update limit
+          </FormButton>
+        ) : (
+          <FormButton type="submit" color="primary">
+            Save limit
+          </FormButton>
+        )}
       </form>
     </div>
   );
