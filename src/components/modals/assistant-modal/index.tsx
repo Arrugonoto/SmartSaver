56d03bff9d1ff3cf9f5efe1d9d23240ef5e09188
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-06-18 00:02:58 +0200

    refactor(assistant-modal): used framer-motion hook 'useAnimation' instead of animated element to create complex animations(and fancy)

--- src/components/modals/assistant-modal/index.tsx
+++ src/components/modals/assistant-modal/index.tsx
@@ -1,38 +1,48 @@
 'use client';
 import { useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, useAnimate } from 'framer-motion';
 import { asistantIcons } from '@lib/constants/icons';
 import { Button } from '@nextui-org/button';
 
 export const AssistantModal = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [scope, animate] = useAnimate();
+
+  const handleAnimate = async () => {
+    if (!isOpen) {
+      await animate(
+        scope.current,
+        { maxHeight: '500px', borderRadius: '6px' },
+        { duration: 0.3 }
+      );
+      await animate(scope.current, { maxWidth: '380px' }, { duration: 0.3 });
+    } else {
+      await animate(scope.current, { maxWidth: '50px' }, { duration: 0.3 });
+      await animate(
+        scope.current,
+        { maxHeight: '50px', borderRadius: '50%' },
+        { duration: 0.3 }
+      );
+    }
+  };
 
   return (
-    <motion.div
-      initial={{
-        right: '30px',
-        bottom: '30px',
-        width: '100%',
-        height: '100%',
-        borderRadius: '50%',
-      }}
-      animate={{
-        maxWidth: isOpen ? '380px' : '50px',
-        maxHeight: isOpen ? '500px' : '50px',
-        borderRadius: isOpen ? '6px' : '50%',
-      }}
-      transition={{ duration: 0.4 }}
-      className={`absolute right-[30px] bottom-[30px] w-[50px] h-[50px] bg-content1 shadow-medium z-[999]`}
+    <motion.section
+      ref={scope}
+      className={`absolute right-[30px] bottom-[30px] max-w-[50px] max-h-[50px] w-full h-full bg-content1 shadow-medium rounded-full z-[999]`}
     >
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[50px] h-[50px] rounded-full overflow-hidden">
         <Button
-          className="p-0 w-full h-full bg-blue-400 rounded-full"
+          className="p-0 w-full h-full bg-transparent rounded-full"
           isIconOnly
-          onPress={() => setIsOpen(!isOpen)}
+          onPress={() => {
+            handleAnimate();
+            setIsOpen(!isOpen);
+          }}
         >
           <asistantIcons.message className="w-6 h-6" />
         </Button>
       </div>
-    </motion.div>
+    </motion.section>
   );
 };
