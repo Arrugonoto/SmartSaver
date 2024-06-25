46be4273574aa6e8f100e50b10b5b1ba4adb480f
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-06-25 21:25:11 +0200

    refactor(auth/register): refactor

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -3,25 +3,25 @@ import { NextApiResponse } from 'next';
 import { createUser } from '@lib/actions/user/create-user';
 
 export async function POST(req: Request, res: NextApiResponse) {
-   try {
-      const formData = await req.json();
-      // validate email and password - Zod will be used here
+  try {
+    const formData = await req.json();
+    // validate email and password - Zod will be used here
 
-      const res = await createUser(formData);
-      if (!res?.error)
-         return NextResponse.json(
-            { message: 'Account created succesfully' },
-            { status: 200 }
-         );
-      return NextResponse.json({ message: res.message }, { status: 500 });
-   } catch (error) {
-      console.error('Sign-up error:', error);
+    const res = await createUser(formData);
+    if (!res?.error)
       return NextResponse.json(
-         {
-            error: error,
-            message: 'Internal Server Error. Registration failed.',
-         },
-         { status: 500 }
+        { message: 'Account created succesfully' },
+        { status: 200 }
       );
-   }
+    return NextResponse.json({ message: res.message }, { status: 500 });
+  } catch (error) {
+    console.error('Sign-up error:', error);
+    return NextResponse.json(
+      {
+        error: error,
+        message: `Couldn't create account now. Try again later.`,
+      },
+      { status: 500 }
+    );
+  }
 }
