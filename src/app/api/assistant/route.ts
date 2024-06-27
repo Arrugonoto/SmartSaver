2f28cff4e334e71bea1278f828034202d0920c4c
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-06-27 13:07:18 +0200

    update(api/assistant): api response now returns data about thread_id and list of messages in thread

--- src/app/api/assistant/route.ts
+++ src/app/api/assistant/route.ts
@@ -4,7 +4,7 @@ import OpenAI from 'openai';
 const openai = new OpenAI({ apiKey: process.env.OPEN_AI_KEY as string });
 
 export async function POST(req: Request) {
-  const { prompt } = await req.json();
+  const { prompt, threadId } = await req.json();
 
   if (!prompt) {
     return NextResponse.json({ error: 'Message is required' }, { status: 400 });
@@ -13,20 +13,31 @@ export async function POST(req: Request) {
   console.log(prompt);
 
   try {
-    const thread = await openai.beta.threads.create();
-    const message = await openai.beta.threads.messages.create(thread.id, {
+    let thread_id = '';
+
+    // Check if thread exists (thread_id),
+    // create new thread if thread_id isn't provided
+    if (!threadId) {
+      const thread = await openai.beta.threads.create();
+      thread_id = thread.id;
+    } else {
+      thread_id = threadId;
+    }
+
+    // Create new user message (prompt)
+    const message = await openai.beta.threads.messages.create(thread_id, {
       role: 'user',
       content: prompt,
     });
 
-    const run = openai.beta.threads.runs.stream(thread.id, {
+    const run = openai.beta.threads.runs.stream(thread_id, {
       assistant_id: process.env.ASSISTANT_ID as string,
     });
 
     // Accumulate response data
-    // Use the stream SDK helper to create a run with
+    // Use the stream OpenAI SDK helper to create a run with
     // streaming. The SDK provides helpful event listeners to handle
-    // the streamed response.
+    // the streamed response. - OpenAI Docs
     let responseText = '';
     run
       .on('textCreated', (text) => (responseText += '\nassistant > '))
@@ -51,10 +62,24 @@ export async function POST(req: Request) {
         }
       });
 
-    // Wait for the response to complete
+    // Wait for the response to complete, and return it
     await new Promise<void>((resolve) => run.on('end', () => resolve()));
-    console.log(responseText);
-    return NextResponse.json({ response: responseText }, { status: 200 });
+
+    // Get thread related messages
+    const thread_messages = await openai.beta.threads.messages.list(thread_id);
+
+    // Extract list of messages from OpenAI API response
+    const messages = thread_messages.data.map((msg) => ({
+      id: msg.id,
+      role: msg.role,
+      content: msg.content,
+    }));
+
+    // Return structured response via Next API route
+    return NextResponse.json(
+      { response: responseText, thread_id, messages },
+      { status: 200 }
+    );
   } catch (error) {
     return NextResponse.json({ error: error }, { status: 500 });
   }
