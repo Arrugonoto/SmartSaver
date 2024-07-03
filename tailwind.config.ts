71df2ed12ca530d5a8a526f9778e135bb060d120
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-07-03 21:40:02 +0200

    update(tailwind-config): added custom animation for loader

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,22 +2,45 @@ import type { Config } from 'tailwindcss';
 import { nextui } from '@nextui-org/react';
 
 const config: Config = {
-   content: [
-      './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-      './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-      './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-      './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-   ],
-   theme: {
-      extend: {
-         backgroundImage: {
-            'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-            'gradient-conic':
-               'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-         },
-         darkMode: 'class',
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-   },
-   plugins: [nextui()],
+      darkMode: 'class',
+      keyframes: {
+        'loading-dots': {
+          '0%, 100%': {
+            'background-size':
+              'calc(100%/3) 100%, calc(100%/3) 100%, calc(100%/3) 100%',
+          },
+          '33%': {
+            'background-size':
+              'calc(100%/3) 0%, calc(100%/3) 100%, calc(100%/3) 100%',
+          },
+          '50%': {
+            'background-size':
+              'calc(100%/3) 100%, calc(100%/3) 0%, calc(100%/3) 100%',
+          },
+          '66%': {
+            'background-size':
+              'calc(100%/3) 100%, calc(100%/3) 100%, calc(100%/3) 0%',
+          },
+        },
+      },
+      animation: {
+        'loading-dots': 'loading-dots 1s infinite linear',
+      },
+    },
+  },
+  plugins: [nextui()],
 };
 export default config;
