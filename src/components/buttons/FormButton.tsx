4c66de701f1d0caf92a4fe0df3899cb49d3f5505
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-07-04 21:07:18 +0200

    update(formButton): added custom prop for only icon version

--- src/components/buttons/FormButton.tsx
+++ src/components/buttons/FormButton.tsx
@@ -3,41 +3,44 @@ import { Button } from '@nextui-org/button';
 import { useFormStatus } from 'react-dom';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-   children: React.ReactNode;
-   onPress?: () => void;
-   isDisabled?: boolean;
-   color?:
-      | 'default'
-      | 'primary'
-      | 'secondary'
-      | 'success'
-      | 'warning'
-      | 'danger'
-      | undefined;
+  children: React.ReactNode;
+  onPress?: () => void;
+  isDisabled?: boolean;
+  color?:
+    | 'default'
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'warning'
+    | 'danger'
+    | undefined;
+  isIconOnly?: boolean;
 }
 
 const FormButton = ({
-   children,
-   className,
-   type,
-   onPress,
-   isDisabled,
-   color,
+  children,
+  className,
+  type,
+  onPress,
+  isDisabled,
+  color,
+  isIconOnly,
 }: ButtonProps) => {
-   const { pending } = useFormStatus();
+  const { pending } = useFormStatus();
 
-   return (
-      <Button
-         radius="full"
-         type={type ?? 'button'}
-         className={`w-full ${className}`}
-         onPress={() => onPress && onPress()}
-         isDisabled={isDisabled || pending}
-         color={color}
-      >
-         {children}
-      </Button>
-   );
+  return (
+    <Button
+      radius="full"
+      type={type ?? 'button'}
+      className={`w-full ${className}`}
+      onPress={() => onPress && onPress()}
+      isDisabled={isDisabled || pending}
+      color={color}
+      isIconOnly={isIconOnly}
+    >
+      {children}
+    </Button>
+  );
 };
 
 export default FormButton;
