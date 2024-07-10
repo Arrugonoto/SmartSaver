571f3bf29ff2ed38482250aae585b232aa30492e
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-07-10 21:23:27 +0200

    refactor(side-menu): updated links

--- src/components/menus/side-menu/index.tsx
+++ src/components/menus/side-menu/index.tsx
@@ -1,28 +1,36 @@
 import Link from 'next/link';
 
 export const SideMenu = () => {
-   return (
-      <div className="flex flex-col min-w-[240px]">
-         <nav className="p-4">
-            <ul>
-               <li>
-                  <Link
-                     href="/dashboard"
-                     className="flex p-2 rounded-md hover:bg-gray-600/50"
-                  >
-                     Overview
-                  </Link>
-               </li>
-               <li>
-                  <Link
-                     href="/dashboard/expenses"
-                     className="flex p-2 rounded-md hover:bg-gray-600/50"
-                  >
-                     Expenses
-                  </Link>
-               </li>
-            </ul>
-         </nav>
-      </div>
-   );
+  return (
+    <div className="flex flex-col min-w-[240px]">
+      <nav className="p-4">
+        <ul>
+          <li>
+            <Link
+              href="/dashboard"
+              className="flex p-2 rounded-md hover:bg-gray-600/50"
+            >
+              Overview
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/expenses"
+              className="flex p-2 rounded-md hover:bg-gray-600/50"
+            >
+              Expenses
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/assistant"
+              className="flex p-2 rounded-md hover:bg-gray-600/50"
+            >
+              Assistant
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
