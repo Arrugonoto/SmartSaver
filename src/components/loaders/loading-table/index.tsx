fb22cf5403294b279c477df35393aebece3fe841
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-07-17 21:26:58 +0200

    fix(loading-table): fixed issue with responsiveness

--- src/components/loaders/loading-table/index.tsx
+++ src/components/loaders/loading-table/index.tsx
@@ -1,18 +1,23 @@
 import { Skeleton } from '@nextui-org/skeleton';
 import { Card } from '@nextui-org/card';
 
-export const LoadingTable = () => {
+export const LoadingTable = ({ sm }: { sm?: boolean }) => {
+  const rows = sm ? 5 : 10;
+
   return (
-    <article className="w-full">
+    <article className="w-full h-full">
       <Card className="p-4 h-full gap-6">
         <div className="flex justify-between">
-          <Skeleton className="h-10 w-1/4 rounded-md" />
-          <Skeleton className="h-10 w-1/4 rounded-md" />
+          <Skeleton className={`${sm ? 'h-6' : 'h-10'}  w-1/4 rounded-md`} />
+          <Skeleton className={`${sm ? 'h-6' : 'h-10'}  w-1/4 rounded-md`} />
         </div>
         <Skeleton className="h-16 rounded-md" />
-        <div className="flex flex-col h-full justify-between">
-          {[...Array(10)].map((el, i) => (
-            <Skeleton key={i} className="h-8 rounded-md" />
+        <div className="flex flex-col h-full justify-between overflow-y-scroll">
+          {[...Array(rows)].map((el, i) => (
+            <Skeleton
+              key={i}
+              className={` ${sm ? 'h-6' : 'h-8'}  rounded-md`}
+            />
           ))}
         </div>
       </Card>
