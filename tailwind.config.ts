bd688ae1c7064768301821699f5d7638bc7710f6
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-07-21 18:21:54 +0200

    update(tailwing.config): added custom breakpoints

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,15 @@ const config: Config = {
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      xs: '420px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+      wide: '1920px',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
