f91e3982a70362f83d8af1c95a981a23284e9239
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-07-26 19:22:11 +0200

    rearrange(user-settings): added page with user account settings

---
+++ src/app/user-settings/page.tsx
@@ -0,0 +1,19 @@
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@app/api/auth/[...nextauth]/options';
+import { Metadata } from 'next';
+import { UserSettingsSection } from '@components/sections/settings-section';
+import type { Session } from 'next-auth';
+
+export const metadata: Metadata = {
+  title: 'User settings',
+};
+
+export default async function Dashboard() {
+  const userSession = await getServerSession(authOptions);
+
+  return (
+    <main className="flex flex-col w-full h-full overflow-auto px-4 py-3 rounded-md">
+      <UserSettingsSection userSession={userSession as Session} />
+    </main>
+  );
+}
