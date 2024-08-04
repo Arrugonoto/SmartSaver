9856ddb162a79f246854dc3c929df68cef6e02f0
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-08-04 18:08:56 +0200

    refactor(expenses): changed data types for more suitable(improved normalization)

--- src/lib/constants/types/expenses/expenses.ts
+++ src/lib/constants/types/expenses/expenses.ts
@@ -1,21 +1,46 @@
 type PaymentType = 'one-time' | 'monthly' | 'subscription' | '';
 
-export type Expense = {
+export interface Expenses {
+  expenses: SingleExpense[];
+  subscriptions: Subscription[];
+}
+
+// export interface Expense {
+//   id: string;
+//   user_id: string;
+//   name: string;
+//   amount: number;
+//   expense_type: string;
+//   payment_type: PaymentType;
+//   description?: string;
+//   created_at: Date;
+//   updated_at?: Date;
+// }
+
+export interface ExpenseCategories {
+  label: string;
+  value: string;
+  color: string;
+}
+
+export interface SingleExpense {
   id: string;
   user_id: string;
   name: string;
   amount: number;
   expense_type: string;
-  payment_type: PaymentType;
   description?: string;
   created_at: Date;
   updated_at?: Date;
-};
+  payment_duration?: number;
+}
 
-export type ExpenseIdRequired = Expense & { id: string };
+// export interface MonthlyPayment extends SingleExpense {
+//   payment_duration: number;
+// }
 
-export type ExpenseCategories = {
-  label: string;
-  value: string;
-  color: string;
-};
+export interface Subscription extends SingleExpense {
+  payment_duration: number;
+}
+
+export type ExpenseIdRequired = SingleExpense & { id: string };
