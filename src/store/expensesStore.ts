38929ad7830a437673929e6a181fb15345af11f9
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-08-04 18:07:05 +0200

    refactor(expensesStore): updated naming convention and added new data types

--- src/store/expensesStore.ts
+++ src/store/expensesStore.ts
@@ -1,42 +1,42 @@
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
-import type { Expense } from '@lib/constants/types/expenses/expenses';
+import type { Expenses } from '@lib/constants/types/expenses/expenses';
 
 type State = {
   userId: string;
-  expenses: Expense[];
+  spendings: Expenses;
   page: number;
   resultsPerPage: number;
   totalResults: number;
-  loadingExpenses: boolean;
+  loadingSpendings: boolean;
 };
 
 type Action = {
   setUserId: (userId: State['userId']) => void;
-  setExpenses: (expenses: State['expenses']) => void;
+  setExpenses: (spendings: State['spendings']) => void;
   setPage: (page: State['page']) => void;
   setResultsPerPage: (resultsPerPage: State['resultsPerPage']) => void;
   setTotalResults: (totalResults: State['totalResults']) => void;
-  setLoadingExpenses: (loadingExpenses: State['loadingExpenses']) => void;
+  setLoadingExpenses: (loadingExpenses: State['loadingSpendings']) => void;
 };
 
 export const useExpensesStore = create(
   persist<State & Action>(
     (set, get) => ({
       userId: '',
-      expenses: [],
+      spendings: { expenses: [], subscriptions: [] },
       page: 1,
       resultsPerPage: 20,
       totalResults: 0,
-      loadingExpenses: true,
+      loadingSpendings: true,
       setUserId: (id) => set({ userId: id }),
-      setExpenses: (newExpenses) => set({ expenses: newExpenses }),
+      setExpenses: (newSpendings) => set({ spendings: newSpendings }),
       setPage: (pageNum) => set({ page: pageNum }),
       setResultsPerPage: (resPerPage) =>
         set(() => ({ resultsPerPage: resPerPage })),
       setTotalResults: (numOfTotal) => set({ totalResults: numOfTotal }),
       setLoadingExpenses: (loadingState) =>
-        set({ loadingExpenses: loadingState }),
+        set({ loadingSpendings: loadingState }),
     }),
     {
       name: 'expenses-storage',
