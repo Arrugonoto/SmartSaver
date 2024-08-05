fd524f5844d2fc6979c50ea07be6849f56b1a474
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-08-05 20:27:54 +0200

    update(definitions): updated database difinitions

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,5 +1,7 @@
 // Type definitions for Tables inside of Database
 
+type PaymentType = 'one-time' | 'monthly' | 'subscription' | '';
+
 export type User = {
   id: string;
   name: string;
@@ -21,8 +23,13 @@ export type Expense = {
   name: string;
   amount: number;
   expense_type: string;
-  payment_type: 'one-time' | 'monthly' | 'subscription';
+  payment_type: PaymentType;
   description?: string;
   created_at: Date;
   updated_at?: Date;
+  payment_duration?: number;
 };
+
+export interface Subscription extends Expense {
+  payment_duration: number;
+}
