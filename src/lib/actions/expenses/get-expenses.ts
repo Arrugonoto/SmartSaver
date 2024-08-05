38aed0725b8acfff9bfc6d2e8540cec0c0b76584
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-08-05 20:46:14 +0200

    udpate(get-expenses): updated get expenses action to match new database structure

--- src/lib/actions/expenses/get-expenses.ts
+++ src/lib/actions/expenses/get-expenses.ts
@@ -1,6 +1,6 @@
 'use server';
 import { sql } from '@vercel/postgres';
-import type { Expense } from '@lib/definitions';
+import type { Expense, Subscription } from '@lib/definitions';
 import { unstable_noStore as noStore } from 'next/cache';
 
 export async function getExpenses(user_id: string) {
@@ -8,24 +8,47 @@ export async function getExpenses(user_id: string) {
 
   try {
     const userExpenses = await sql<Expense>`
-      SELECT id, name, amount, expense_type, payment_type, description, created_at, updated_at 
+      SELECT id, name, amount, expense_type, payment_type, description, created_at, updated_at, payment_duration 
       FROM expenses 
       WHERE user_id=${user_id}
       `;
 
-    const totalResults = await sql`
+    const userSubscriptions = await sql<Subscription>`
+    SELECT id, name, amount, expense_type, payment_type, description, created_at, updated_at, payment_duration
+    FROM subscriptions
+    WHERE user_id=${user_id}
+    `;
+
+    const numOfExpenses = await sql`
       SELECT COUNT(*) AS total_results
       FROM expenses
       WHERE user_id = ${user_id}
       `;
 
+    const numOfSubscriptions = await sql`
+    SELECT COUNT(*) AS total_results
+    FROM subscriptions
+    WHERE user_id = ${user_id}
+    `;
+
     if (userExpenses.rows.length === 0) {
-      return { message: `You have no expenses to keep track of` };
+      return { message: `You have no expenses to keep track of.` };
     }
 
+    if (userSubscriptions.rows.length === 0) {
+      return { message: `You have no subscriptions to keep track of.` };
+    }
+
+    const totalResults =
+      numOfExpenses.rows[0].total_result +
+      numOfSubscriptions.rows[0].total_result;
+
     return {
-      data: userExpenses.rows,
-      totalResults: totalResults.rows[0].total_results,
+      data: {
+        expenses: userExpenses.rows,
+        subscriptions: userSubscriptions.rows,
+      },
+      totalResults: totalResults,
       status: 'success',
     };
   } catch (error) {
