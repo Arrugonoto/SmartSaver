212eb76104e9acaf3f34e19701f19a99d38e81a5
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-08-17 21:11:51 +0200

    rearrange(filter-current/ last-spendings): added helpers for stable filtering of current/last month active spendings

---
+++ src/lib/helpers/filter-current-month-spendings.ts
@@ -0,0 +1,64 @@
+import { Expenses } from '@lib/constants/types/expenses/expenses';
+export const filterCurrentMonthData = (data: Expenses, currentDate: Date) => {
+  // function for filtering data based on expense creation and duration dates
+  const currentYear = currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth();
+
+  const filterSingleSpendings = data?.expenses
+    .filter((expense) => expense.payment_type === 'one-time')
+    .map((spending) => {
+      const expenseDate = new Date(spending.created_at);
+      const yearOfExpense = expenseDate.getFullYear();
+      const monthOfExpense = expenseDate.getMonth();
+
+      if (yearOfExpense === currentYear && monthOfExpense === currentMonth) {
+        return spending;
+      }
+    })
+    .filter((expense) => expense !== undefined);
+
+  const filterMonthlySpendings = data?.expenses
+    .filter((expense) => expense.payment_type === 'monthly')
+    .map((spending) => {
+      const startOfPayment = new Date(spending.created_at);
+      const lastPayment = new Date(
+        startOfPayment.setMonth(
+          startOfPayment.getMonth() + spending.payment_duration!
+        )
+      );
+
+      const lastPaymentDate = new Date(
+        lastPayment.getFullYear(),
+        lastPayment.getMonth()
+      );
+
+      if (lastPaymentDate >= new Date(currentYear, currentMonth))
+        return spending;
+    })
+    .filter((spending) => spending !== undefined);
+
+  const filterSubscriptions = data?.subscriptions
+    .map((subscription) => {
+      const startOfSub = new Date(subscription.created_at);
+      const lastPayment = new Date(
+        startOfSub.setMonth(
+          startOfSub.getMonth() + subscription.payment_duration!
+        )
+      );
+
+      const lastPaymentDate = new Date(
+        lastPayment.getFullYear(),
+        lastPayment.getMonth()
+      );
+
+      if (lastPaymentDate >= new Date(currentYear, currentMonth))
+        return subscription;
+    })
+    .filter((subscription) => subscription !== undefined);
+
+  return [
+    ...filterSingleSpendings,
+    ...filterMonthlySpendings,
+    ...filterSubscriptions,
+  ];
+};
---
+++ src/lib/helpers/filter-last-month-spendings.ts
@@ -0,0 +1,85 @@
+import { Expenses } from '@lib/constants/types/expenses/expenses';
+
+export const filterLastMonthData = (data: Expenses, currentDate: Date) => {
+  const currentMonth = currentDate.getMonth();
+  const currentYear = currentDate.getFullYear();
+  const previousMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+  const previousYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+
+  // get last month and last year as date
+  const lastMonth = new Date(previousYear, previousMonth);
+
+  const filterLastMonthSingleSpendings = data?.expenses
+    .filter((expense) => expense.payment_type === 'one-time')
+    .map((spending) => {
+      const expenseDate = new Date(spending.created_at);
+      const expenseMonthAndYear = new Date(
+        expenseDate.getFullYear(),
+        expenseDate.getMonth()
+      );
+
+      if (expenseMonthAndYear.getTime() === lastMonth.getTime()) {
+        return spending;
+      }
+    })
+    .filter((expense) => expense !== undefined);
+
+  const filterLastMonthSpendings = data?.expenses
+    .filter(
+      (expense) =>
+        expense.payment_type === 'monthly' &&
+        new Date(
+          new Date(expense.created_at).getFullYear(),
+          new Date(expense.created_at).getMonth()
+        ) <= lastMonth
+    )
+    .map((spending) => {
+      const startOfPayment = new Date(spending.created_at);
+      const lastPayment = new Date(
+        startOfPayment.setMonth(
+          startOfPayment.getMonth() + spending.payment_duration!
+        )
+      );
+
+      const lastPaymentDate = new Date(
+        lastPayment.getFullYear(),
+        lastPayment.getMonth()
+      );
+
+      if (lastPaymentDate >= new Date(currentYear, currentMonth))
+        return spending;
+    })
+    .filter((spending) => spending !== undefined);
+
+  const filterLastMonthSubscriptions = data?.subscriptions
+    .filter(
+      (subscription) =>
+        new Date(
+          new Date(subscription.created_at).getFullYear(),
+          new Date(subscription.created_at).getMonth()
+        ) <= lastMonth
+    )
+    .map((subscription) => {
+      const startOfSub = new Date(subscription.created_at);
+      const lastPayment = new Date(
+        startOfSub.setMonth(
+          startOfSub.getMonth() + subscription.payment_duration!
+        )
+      );
+
+      const lastPaymentDate = new Date(
+        lastPayment.getFullYear(),
+        lastPayment.getMonth()
+      );
+
+      if (lastPaymentDate >= new Date(currentYear, currentMonth))
+        return subscription;
+    })
+    .filter((subscription) => subscription !== undefined);
+
+  return [
+    ...filterLastMonthSingleSpendings,
+    ...filterLastMonthSpendings,
+    ...filterLastMonthSubscriptions,
+  ];
+};
