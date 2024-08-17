b4cc52ebb7643ea592055b39e06a318bd5b7830c
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-08-17 21:04:30 +0200

    rearrange(get-total-spendings-so-far): added function for stable calculation of total spendings so far

---
+++ src/lib/helpers/get-total-spendings-so-far.ts
@@ -0,0 +1,79 @@
+import { Expenses } from '@lib/constants/types/expenses/expenses';
+
+export const calcTotalExpensesSoFar = (spendings: Expenses) => {
+  // declare arrays which will hold values of total spendings, and subscriptions so far of monthly payments
+  const monthlyPaymentsSoFar: number[] = [];
+  const subscriptionsSoFar: number[] = [];
+
+  const singleSpendings = spendings?.expenses.filter(
+    (expense) => expense.payment_type === 'one-time'
+  );
+
+  const monthlySpendings = spendings?.expenses.filter(
+    (expense) => expense.payment_type === 'monthly'
+  );
+
+  const calcMonthlySoFar = monthlySpendings.map((expense) => {
+    // Calculate difference in month from start of payment, to current date
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const startYear = new Date(expense.created_at).getFullYear();
+    const startMonth = new Date(expense.created_at).getMonth();
+
+    const yearDiff = (currentYear - startYear) * 12; // multiply year with 12 months
+    const monthDiff = currentMonth - startMonth + 1; // payment always starts from first month, add 1 to include first payment on start
+
+    const totalMonthDiff = yearDiff + monthDiff;
+
+    if (totalMonthDiff <= (expense.payment_duration as number)) {
+      monthlyPaymentsSoFar.push(
+        totalMonthDiff * parseFloat(expense.amount as any)
+      );
+    } else {
+      monthlyPaymentsSoFar.push(
+        (expense.payment_duration as number) * parseFloat(expense.amount as any)
+      );
+    }
+  });
+
+  const calcSubscriptionsSoFar = spendings.subscriptions.map((subscription) => {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const startYear = new Date(subscription.created_at).getFullYear();
+    const startMonth = new Date(subscription.created_at).getMonth();
+
+    const yearDiff = (currentYear - startYear) * 12;
+    const monthDiff = currentMonth - startMonth + 1;
+
+    const totalMonthDiff = yearDiff + monthDiff;
+
+    if (totalMonthDiff <= (subscription.payment_duration as number)) {
+      subscriptionsSoFar.push(
+        totalMonthDiff * parseFloat(subscription.amount as any)
+      );
+    } else {
+      subscriptionsSoFar.push(
+        (subscription.payment_duration as number) *
+          parseFloat(subscription.amount as any)
+      );
+    }
+  });
+
+  const totalSingle = singleSpendings.reduce(
+    (sum, expense) => sum + parseFloat(expense.amount as any),
+    0
+  );
+
+  const totalMonthly = monthlyPaymentsSoFar.reduce(
+    (sum, amount) => sum + amount,
+    0
+  );
+
+  const totalSubscriptions = subscriptionsSoFar.reduce(
+    (sum, amount) => sum + amount,
+    0
+  );
+
+  const totalSpendingsSoFar = totalSingle + totalMonthly + totalSubscriptions;
+  return totalSpendingsSoFar;
+};
