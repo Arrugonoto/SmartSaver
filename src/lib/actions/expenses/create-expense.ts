d92507dba039e5edd7497d806526f2c8ed733098
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-08-27 21:05:50 +0200

    update(create-expense): updated action according to the new database structure

--- src/lib/actions/expenses/create-expense.ts
+++ src/lib/actions/expenses/create-expense.ts
@@ -1,51 +1,75 @@
 'use server';
 import { sql } from '@vercel/postgres';
 import { z } from 'zod';
-import type { Expense } from '@lib/definitions';
+import type { Expense, Subscription } from '@lib/definitions';
 
 const ExpenseSchema = z.object({
-   user_id: z.string(),
-   name: z.string(),
-   amount: z.number(),
-   expense_type: z.string(),
-   payment_type: z.string(),
-   description: z.string().optional(),
+  user_id: z.string(),
+  name: z.string(),
+  amount: z.number(),
+  expense_type: z.string(),
+  payment_type: z.string(),
+  payment_duration: z.number().optional(),
+  description: z.string().optional(),
 });
 
 export async function createExpense(formData: {
-   user_id: string;
-   name: string;
-   amount: number;
-   expense_type: string;
-   payment_type: string;
-   description?: string;
+  user_id: string;
+  name: string;
+  amount: number;
+  expense_type: string;
+  payment_type: string;
+  description?: string;
+  payment_duration?: number;
 }) {
-   const validatedFields = ExpenseSchema.safeParse({
-      user_id: formData.user_id,
-      name: formData.name,
-      amount: formData.amount,
-      expense_type: formData.expense_type,
-      payment_type: formData.payment_type,
-      description: formData.description || '',
-   });
+  const validatedFields = ExpenseSchema.safeParse({
+    user_id: formData.user_id,
+    name: formData.name,
+    amount: formData.amount,
+    expense_type: formData.expense_type,
+    payment_type: formData.payment_type,
+    description: formData.description || '',
+    payment_duration: formData.payment_duration,
+  });
 
-   try {
-      if (!validatedFields.success) {
-         throw new Error('Fill all necessary fields.');
-      }
+  try {
+    if (!validatedFields.success) {
+      throw new Error('Fill all necessary fields.');
+    }
 
-      const { user_id, name, amount, expense_type, payment_type, description } =
-         validatedFields.data;
+    const {
+      user_id,
+      name,
+      amount,
+      expense_type,
+      payment_type,
+      description,
+      payment_duration,
+    } = validatedFields.data;
 
-      const timeOfCreation = new Date().toISOString();
+    const timeOfCreation = new Date().toISOString();
 
+    if (payment_type === 'one-time') {
       const createNewExpense = await sql<Expense>`
       INSERT INTO expenses (user_id, name, amount, expense_type, payment_type, description, created_at)
       VALUES (${user_id}, ${name}, ${amount}, ${expense_type}, ${payment_type}, ${description} , ${timeOfCreation})
       `;
-   } catch (error) {
-      const errorMessage = error instanceof Error && error.message;
-      console.log(error);
-      return { error: error, message: errorMessage };
-   }
+    }
+    if (payment_type === 'monthly') {
+      const createNewExpense = await sql<Expense>`
+      INSERT INTO expenses (user_id, name, amount, expense_type, payment_type, description, created_at, payment_duration)
+      VALUES (${user_id}, ${name}, ${amount}, ${expense_type}, ${payment_type}, ${description} , ${timeOfCreation}, ${payment_duration})
+      `;
+    }
+    if (payment_type === 'subscription') {
+      const createNewExpense = await sql<Subscription>`
+      INSERT INTO subscriptions (user_id, name, amount, expense_type, payment_type, description, created_at, payment_duration)
+      VALUES (${user_id}, ${name}, ${amount}, ${expense_type}, ${payment_type}, ${description} , ${timeOfCreation}, ${payment_duration})
+      `;
+    }
+  } catch (error) {
+    const errorMessage = error instanceof Error && error.message;
+    console.log(error);
+    return { error: error, message: errorMessage };
+  }
 }
