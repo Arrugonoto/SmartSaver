dbc3fc6edf60626591c9d85853a25e1dd84af772
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-09-11 23:47:33 +0200

    UPDATE(overview): added conditional render based on available tracking list data

--- src/components/overview/index.tsx
+++ src/components/overview/index.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import type { Expenses } from '@constants/types/expenses/expenses';
 import { getExpenses } from '@lib/actions/expenses/get-expenses';
 import { useFetch } from '@lib/hooks/useFetch';
@@ -8,6 +8,8 @@ import { ExpensesSummarySection } from '@components/sections/overview/summary';
 import { MonthlyChartsSection } from '@components/sections/overview/monthly-charts';
 import { AnnualChartSection } from '@components/sections/overview/annual-chart';
 import { CreateExpenseModal } from '@components/modals/create-expense-modal';
+import { Card, CardHeader, CardBody } from '@nextui-org/card';
+import { infoIcons } from '@lib/constants/icons';
 
 export const ExpensesOverview = ({ user_id }: { user_id: string }) => {
   const { setSpendings, setTotalResults, spendings } = useExpensesStore(
@@ -17,11 +19,16 @@ export const ExpensesOverview = ({ user_id }: { user_id: string }) => {
       spendings: state.spendings,
     })
   );
+
   const { data, totalResults, isLoading } = useFetch<Expenses>({
     action: getExpenses,
     user_id,
     initialFetch: true,
   });
+
+  const dataNotAvailable =
+    !spendings ||
+    (spendings.expenses.length === 0 && spendings.subscriptions.length === 0);
 
   useEffect(() => {
     if (
@@ -29,7 +36,6 @@ export const ExpensesOverview = ({ user_id }: { user_id: string }) => {
       spendings.subscriptions.length === 0
     ) {
       if (data) setSpendings(data);
-      console.log('initial fetch fired');
     }
   }, [spendings, setSpendings, data]);
 
@@ -43,13 +49,29 @@ export const ExpensesOverview = ({ user_id }: { user_id: string }) => {
         <h1 className="text-2xl mb-4">Expenses overview</h1>
         <CreateExpenseModal />
       </div>
-      <div className="flex flex-col gap-6 pb-4">
-        <ExpensesSummarySection spendings={spendings} isLoading={isLoading} />
+      {dataNotAvailable ? (
+        <div className="flex w-full h-full justify-center items-center">
+          <Card className="p-4">
+            <CardBody className="flex-row gap-4 items-start">
+              <div className="p-1">
+                <infoIcons.informative className="text-2xl text-sky-400" />
+              </div>
+              <div className="flex flex-col gap-4 items-center pt-2">
+                <h2>To enable analytics, add an expense to tracking list</h2>
+                <CreateExpenseModal />
+              </div>
+            </CardBody>
+          </Card>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-6 pb-4">
+          <ExpensesSummarySection spendings={spendings} isLoading={isLoading} />
 
-        <MonthlyChartsSection />
+          <MonthlyChartsSection />
 
-        <AnnualChartSection />
-      </div>
+          <AnnualChartSection />
+        </div>
+      )}
     </div>
   );
 };
