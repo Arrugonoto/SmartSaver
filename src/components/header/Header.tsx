7044d59f8a9c54694e7dc5d458b1c3fa0456fbda
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-09-12 23:46:51 +0200

    FEAT(header): added button for changing color theme

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,9 +10,12 @@ import Link from 'next/link';
 import { useSession } from 'next-auth/react';
 import { UserMenu } from '@components/menus/user-menu';
 import { btnIcons } from '@lib/constants/icons';
+import { useTheme } from 'next-themes';
+import { Tooltip } from '@nextui-org/tooltip';
 
 export const Header = () => {
   const { data: session } = useSession();
+  const { theme, setTheme } = useTheme();
 
   return (
     <header className="sticky top-0 left-0 w-full z-[999]">
@@ -33,14 +36,37 @@ export const Header = () => {
               <UserMenu />
             </NavbarItem>
           ) : (
-            <NavbarItem>
-              <Link href="/login">
-                <Button className="flex items-center px-2">
-                  <btnIcons.signin className="text-base" />
-                  <p>Sign In</p>
-                </Button>
-              </Link>
-            </NavbarItem>
+            <div className="flex gap-2">
+              <NavbarItem>
+                <Link href="/login">
+                  <Button className="flex items-center px-2">
+                    <btnIcons.signin className="text-base" />
+                    <p>Sign In</p>
+                  </Button>
+                </Link>
+              </NavbarItem>
+              <NavbarItem>
+                <Tooltip
+                  content="Toggle color theme"
+                  showArrow
+                  placement="bottom-end"
+                >
+                  <Button
+                    isIconOnly={true}
+                    onPress={() =>
+                      setTheme(theme === 'dark' ? 'light' : 'dark')
+                    }
+                    className="text-lg bg-transparent"
+                  >
+                    {theme === 'light' ? (
+                      <btnIcons.lightMode />
+                    ) : (
+                      <btnIcons.darkMode />
+                    )}
+                  </Button>
+                </Tooltip>
+              </NavbarItem>
+            </div>
           )}
         </NavbarContent>
       </Navbar>
