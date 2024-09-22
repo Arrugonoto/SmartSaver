bffe30ca537201d4e590a6595dfbbd2b87ca4b4c
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-09-22 20:22:30 +0200

    UPDATE(delete-user): updated action for deletion of data, based on email and user id

--- src/lib/actions/user/delete-user.ts
+++ src/lib/actions/user/delete-user.ts
@@ -1,21 +1,27 @@
-' use server';
+'use server';
 import { sql } from '@vercel/postgres';
 
-export async function deleteUser(user_id: string) {
-   try {
-      // Delete user related data before deleting account
-      // Before deleting user delete all created expenses and expense_summary
-      // after that, delete user
-      const deleteExpenses =
-         await sql`DELETE FROM expenses WHERE user_id=${user_id}`;
+export async function deleteUser(formData: { email: string; user_id: string }) {
+  const { user_id, email } = formData;
 
-      const deleteSummary =
-         await sql`DELETE FROM expenses_summary WHERE user_id=${user_id}`;
+  // Delete user related data before deleting account.
+  // Before deleting user delete all created expenses, summary, and budget data,
+  // after that, delete user
+  const deleteExpenses =
+    await sql`DELETE FROM expenses WHERE user_id=${user_id}`;
 
-      const deleteUser = await sql`DELETE FROM users WHERE id=${user_id}`;
-   } catch (error) {
-      const errorMessage = error instanceof Error && error.message;
-      console.log(error);
-      return { error: error, message: errorMessage };
-   }
+  const deleteSubscriptions =
+    await sql`DELETE FROM subscriptions WHERE user_id=${user_id}`;
+
+  const deleteSummary =
+    await sql`DELETE FROM expenses_summary WHERE user_id=${user_id}`;
+
+  const deleteBudget = await sql`DELETE FROM budget WHERE user_id=${user_id}`;
+
+  // PostgreSQL performs DELETE statement on all rows which mmatch condition;
+  // if no rows matches condition, Postgre just reurns info about '0 rows deleted';
+  // so no additional check is required
+
+  const deleteUser = await sql`DELETE FROM users WHERE email=${email}`;
+  return { message: `Account deleted succesfully` };
 }
