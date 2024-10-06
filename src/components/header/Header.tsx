bd4a1c40b8a77e3f773f06dfbb25a2b9f9120c98
Arugonoto <klekot.krzysztof1008@gmail.com>
2024-10-06 16:20:01 +0200

    UPDATE(header): added logo and mobile menu button

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,72 +9,113 @@ import { Button } from '@nextui-org/button';
 import Link from 'next/link';
 import { useSession } from 'next-auth/react';
 import { UserMenu } from '@components/menus/user-menu';
-import { btnIcons } from '@lib/constants/icons';
+import { btnIcons, menuIcons } from '@lib/constants/icons';
 import { useTheme } from 'next-themes';
 import { Tooltip } from '@nextui-org/tooltip';
+import Image from 'next/image';
+import { useMobileMenuConext } from '@context/MobileMenuContext';
 
 export const Header = () => {
   const { data: session } = useSession();
   const { theme, setTheme } = useTheme();
+  const { showMobileMenu, setShowMobileMenu } = useMobileMenuConext();
 
   return (
-    <header className="sticky top-0 left-0 w-full z-[999]">
-      <Navbar
-        className="flex h-12 bg-default-50"
-        isBordered
-        shouldHideOnScroll
-        maxWidth="full"
-      >
-        <NavbarBrand>im a header</NavbarBrand>
-        <NavbarContent justify="end">
-          <NavbarItem>
-            <p>{session && `Hi ${session?.user?.name}`}</p>
-          </NavbarItem>
+    <Navbar
+      isBordered
+      shouldHideOnScroll
+      maxWidth="full"
+      className="flex min-h-12 h-12 bg-default-50 overflow-hidden gap-0"
+      classNames={{
+        wrapper: ['gap-2'],
+      }}
+    >
+      <NavbarItem className="sm:hidden">
+        <Button
+          isIconOnly
+          className={`bg-transparent ${
+            theme === 'dark' ? 'hover:bg-gray-200/20' : 'hover:bg-gray-400/40'
+          } `}
+          onPress={() => {
+            setShowMobileMenu((prev) => !prev);
+          }}
+        >
+          <menuIcons.menu className="text-2xl" />
+        </Button>
+      </NavbarItem>
+      <NavbarBrand className=" overflow-hidden">
+        <Link href="/">
+          <Image
+            src="/images/logo.svg"
+            alt="App logo"
+            width={60}
+            height={60}
+            className="object-cover "
+          />
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden xs:list-item">
+          <p>{session && `Hi ${session?.user?.name}`}</p>
+        </NavbarItem>
 
-          {session ? (
+        {session ? (
+          <NavbarItem>
+            <UserMenu />
+          </NavbarItem>
+        ) : (
+          <div className="flex gap-2 items-center">
             <NavbarItem>
-              <UserMenu />
-            </NavbarItem>
-          ) : (
-            <div className="flex gap-2">
-              <NavbarItem>
-                <Link href="/login">
-                  <Button className="flex items-center px-2">
-                    <btnIcons.signin className="text-base" />
-                    <p>Sign In</p>
-                  </Button>
-                </Link>
-              </NavbarItem>
-              <NavbarItem>
-                <Tooltip
-                  content="Toggle color theme"
-                  showArrow
-                  placement="bottom-end"
+              <Link href="/login">
+                <Button
+                  size="sm"
+                  className="flex items-center px-2 text-sm"
+                  startContent={<btnIcons.signin className="text-base" />}
                 >
-                  <Button
-                    isIconOnly={true}
-                    onPress={() => {
-                      if (!theme) {
-                        setTheme('light');
-                      }
-                      if (theme) {
-                        setTheme(theme === 'dark' ? 'light' : 'dark');
-                      }
-                    }}
-                    className="text-lg bg-transparent"
-                  >
-                    {theme === 'light' ? (
-                      <btnIcons.lightMode />
-                    ) : (
-                      <btnIcons.darkMode />
-                    )}
-                  </Button>
-                </Tooltip>
-              </NavbarItem>
-            </div>
-          )}
-        </NavbarContent>
-      </Navbar>
-    </header>
+                  <p>Sign In</p>
+                </Button>
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Link href="/signup">
+                <Button
+                  size="sm"
+                  variant="light"
+                  className="flex items-center px-2 text-sm"
+                >
+                  <p>Sign Up</p>
+                </Button>
+              </Link>
+            </NavbarItem>
+            <NavbarItem>
+              <Tooltip
+                content="Toggle color mode"
+                showArrow
+                placement="bottom-end"
+              >
+                <Button
+                  isIconOnly={true}
+                  onPress={() => {
+                    if (!theme) {
+                      setTheme('light');
+                    }
+                    if (theme) {
+                      setTheme(theme === 'dark' ? 'light' : 'dark');
+                    }
+                  }}
+                  className="text-lg bg-transparent"
+                >
+                  {theme === 'light' ? (
+                    <btnIcons.lightMode />
+                  ) : (
+                    <btnIcons.darkMode />
+                  )}
+                </Button>
+              </Tooltip>
+            </NavbarItem>
+          </div>
+        )}
+      </NavbarContent>
+    </Navbar>
   );
 };
